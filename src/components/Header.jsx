38ecaacd0c044a4a5ba44e91de449804c9a9610e
Antenor <antenorzapata@gmail.com>
2021-06-08 17:42:23 -0300

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email, expenses } = this.props;
+    return (
+      <div>
+        <nav className="nav-header">
+          <header className="header">
+            <p data-testid="email-field">{email}</p>
+            <p data-testid="total-field">
+              {expenses.length ? this.handleExpensesSum() : 0.00 }
+            </p>
+            <p data-testid="header-currency-field">BRL</p>
+          </header>
+        </nav>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+// import FormExpenses from '../components/FormExpenses';
+import Header from '../components/Header';
+// import ExpensesTable from '../components/ExpensesTable';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        {/* <FormExpenses />
+        <ExpensesTable /> */}
+      </div>
+    );
   }
 }
 
