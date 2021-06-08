d6c1df77721c1b96c40b8d093055d978fac45176
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-08 17:21:31 -0300

    Cria o Roteamento do login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+
+    </Router>
   );
 }
 
