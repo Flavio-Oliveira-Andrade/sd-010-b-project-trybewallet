8236df17915faceb9e69a5e8f08b84c3b82b1793
Renan0211 <renan.antunes.rocha@gmail.com>
2021-06-08 20:25:24 -0300

    Começa a implementaçao de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+export const userLogin = (state) => ({ type: 'USER_LOGIN', state });
+
+export const newExpense = (state) => ({ type: 'NEW_EXPENSE', state });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmailAndPAssword = this.validateEmailAndPAssword.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      disabledButton: true,
+      shouldRedirect: false,
+    };
+  }
+
+  redirect() {
+    this.setState({ shouldRedirect: true });
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.validateEmailAndPAssword();
+  }
+
+  validateEmailAndPAssword() {
+    const idealPasswordLength = 5;
+    const { email, password } = this.state;
+    const emailChecks = email.includes('@') && email.includes('.com');
+    const passwordChecks = password.length >= idealPasswordLength;
+    const result = emailChecks && passwordChecks;
+    this.setState({ disabledButton: !result });
+  }
+
   render() {
+    const { email, disabledButton, shouldRedirect } = this.state;
+    const { dispatchLogin } = this.props;
+    if (shouldRedirect) return <Redirect to="/carteira" />;
     return (
       <div>
         <h2>Login</h2>
@@ -12,7 +51,8 @@ class Login extends React.Component {
               data-testid="email-input"
               type="email"
               id="email-input"
-              name="email-input"
+              name="email"
+              onChange={ this.handleChange }
             />
           </label>
           <br />
@@ -22,14 +62,34 @@ class Login extends React.Component {
               data-testid="password-input"
               type="password"
               id="password-input"
-              name="password-input"
+              name="password"
+              onChange={ this.handleChange }
             />
           </label>
-          <button type="button">Entrar</button>
+          <button
+            type="button"
+            disabled={ disabledButton }
+            onClick={ () => {
+              this.redirect();
+              dispatchLogin(email);
+            } }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  dispatchLogin: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogin: (state) => dispatch(userLogin(state)),
+});
+
+mapDispatchToProps();
 
 export default Login;
