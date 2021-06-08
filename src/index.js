3a4e860cd538e4cf6344d827c60606f82b1011fc
root <diegoserafim1@gmail.com>
2021-06-08 20:40:11 -0300

    criado a rota da pagina Login para a Home principal / e criado o form de login

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
@@ -16,3 +20,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+// #Passo 03
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login">
+        <section className="login-inputs">
+          <input
+            type="text"
+            placeholder="Email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+        </section>
+        <div className="link">
+          <Link
+            to="/"
+            data-testid="btn-login"
+          >
+            Entrar
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
 export default Login;
+// #Passo 01
