e4c4f8f98e981b6c0c2dd43bab32bc18a99311e8
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 17:28:02 -0300

    requisito 2 - validação email e senha, e linkando com a rota wallet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isValidEmail: false,
+      isValidPassword: false,
+    };
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.changeDisabled = this.changeDisabled.bind(this);
+  }
+
+  handleEmail({ target: { value } }) {
+    const emailRegexp = RegExp(/[a-z]+@[a-z]+.com/g);
+    const isValidEmail = emailRegexp.test(value);
+    this.setState({
+      isValidEmail,
+    });
+  }
+
+  handlePassword({ target: { value } }) {
+    const MIN_LENGTH_PASSWORD = 5;
+    if (value.length > MIN_LENGTH_PASSWORD) {
+      this.setState({
+        isValidPassword: true,
+      });
+    }
+  }
+
+  changeDisabled() {
+    const { isValidPassword, isValidEmail } = this.state;
+    return !!((isValidPassword && isValidEmail));
+  }
+
   render() {
     return (
       <div>
-        <input type="text" data-testid="email-input" placeholder="email" />
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="email"
+          onChange={ (e) => this.handleEmail(e) }
+        />
         <br />
-        <input type="password" data-testid="password-input" placeholder="senha" />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password-input"
+          placeholder="senha"
+          onChange={ (e) => this.handlePassword(e) }
+        />
         <br />
-        <button type="button">Entrar</button>
+        <Link to="/wallet">
+          <button type="button" disabled={ !this.changeDisabled() }>Entrar</button>
+        </Link>
       </div>
     );
   }
