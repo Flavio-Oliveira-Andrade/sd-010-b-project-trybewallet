81ff52306f77d04a5e3819970faf86cda5163e3c
Abimael <abimaelr.albu@gmail.com>
2021-06-08 16:21:51 -0300

    1 Feature done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import '../css/login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="loginPage">
+        <div className="loginBox">
+          <h2>Login</h2>
+          <form className="form">
+            <input data-testid="email-input" type="email" placeholder="Email" />
+            <input data-testid="password-input" type="password" placeholder="Senha" />
+            <button type="button">Entrar</button>
+          </form>
+        </div>
+      </div>);
   }
 }
 
