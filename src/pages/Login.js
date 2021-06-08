42ab646a398d5c3e0c5da8ba2dc5946d0e030622
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 15:46:16 -0300

    requisito 1 - adicionando inputs de email e senha, e botão de Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="text" data-testid="email-input" placeholder="email" />
+        <br />
+        <input type="password" data-testid="password-input" placeholder="senha" />
+        <br />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
