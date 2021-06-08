4b5c476d21158257646eaa86a73f524eac2741e4
Pedro Amaral <amaralph1995@gmail.com>
2021-06-08 19:34:16 -0300

    Adiciona validação e redirect ao login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      validEmail: false,
+      validPswd: false,
+      email: '',
+      shouldRedirect: false,
+    };
+  }
+
   render() {
+    const { validEmail, validPswd, email, shouldRedirect } = this.state;
+    const { saveEmail } = this.props;
     return (
       <div>
         <form>
@@ -11,6 +29,11 @@ class Login extends React.Component {
               type="text"
               name="email"
               data-testid="email-input"
+              onChange={ (e) => {
+                const ragex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                const input = e.target.value;
+                this.setState({ validEmail: ragex.test(input), email: input });
+              } }
             />
           </label>
           <label htmlFor="senha">
@@ -19,13 +42,35 @@ class Login extends React.Component {
               type="password"
               name="senha"
               data-testid="password-input"
+              onChange={ (e) => {
+                const minLen = 6;
+                this.setState({ validPswd: e.target.value.length >= minLen });
+              } }
             />
           </label>
         </form>
-        <button type="button">Entrar</button>
+        {shouldRedirect ? <Redirect to="/carteira" /> : null }
+        <button
+          type="button"
+          onClick={ () => {
+            saveEmail(email);
+            this.setState({ shouldRedirect: true });
+          } }
+          disabled={ !(validPswd && validEmail) }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = ({
+  saveEmail: PropTypes.func,
+}).isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
