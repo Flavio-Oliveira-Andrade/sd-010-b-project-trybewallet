4e4842e0cc2b4be49d85b082590fe13c9559d7eb
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-08 19:34:11 -0300

    Inicial form for expenses

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,11 +6,50 @@ class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header data-testid="email-field">
-        <p>{email}</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <>
+        <header data-testid="email-field">
+          <div>{email}</div>
+          <div data-testid="total-field">0</div>
+          <div data-testid="header-currency-field">BRL</div>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="number" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" id="description" />
+          </label>
+
+          <label htmlFor="currency">
+            Moeda:
+            <select id="currency">
+              <option value="">BRL</option>
+            </select>
+          </label>
+
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select id="payment">
+              <option value="paper">Dinheiro</option>
+              <option value="creditCard">Cartão de crédito</option>
+              <option value="debitCard">Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="type">
+            Tag:
+            <select id="type">
+              <option value="food">Alimentação</option>
+              <option value="fun">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transportation">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
