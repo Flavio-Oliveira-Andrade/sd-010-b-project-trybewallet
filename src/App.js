78abb8236b35e0e1ee15c07533187f723decb3c7
Thiago Leite <leite.tjs@gmail.com>
2021-06-08 16:53:54 -0300

    store created and provided

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route to="/" component={ Login } />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route to="/" component={ Login } />
+    </Switch>
   );
 }
 
