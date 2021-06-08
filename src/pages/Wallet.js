0ee49afdb573abeb1a4db14fcca45b07d7df278c
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-06-08 17:57:01 -0300

    desafio 4 e 5 feitos

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        TrybeWallet
+        <header>
+          <p data-testid="email-field">
+            Boas vindas
+            {' '}
+            {email}
+            {' '}
+          </p>
+          {' '}
+          <br />
+          {' '}
+          <div data-testid="total-field">
+            Despesa Total:
+            { 0 }
+            {' '}
+          </div>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+      </div>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
