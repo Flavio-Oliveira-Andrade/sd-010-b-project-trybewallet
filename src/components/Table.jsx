8cf9618a9400f0458d509dfc40a453e17df8a7c9
Caio <thehaseol@gmail.com>
2021-06-08 19:28:49 -0300

    Correçoes do Lint 1.0

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { number, arrayOf, shape } from "prop-types";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { number, arrayOf, shape } from 'prop-types';
 
 class Table extends Component {
   constructor(props) {
@@ -11,37 +11,37 @@ class Table extends Component {
   render() {
     const { expenses } = this.props;
     const headers = [
-      "Descrição",
-      "Tag",
-      "Método de pagamento",
-      "Valor",
-      "Moeda",
-      "Câmbio utilizado",
-      "Valor convertido",
-      "Moeda de conversão",
-      "Editar/Excluir",
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
     ];
     return (
       <table>
         <tr>
           {headers.map((th) => (
-            <th key={th}>{th}</th>
+            <th key={ th }>{ th }</th>
           ))}
         </tr>
         {expenses.map((expense) => (
-          <tr key={expense.id}>
-            <td>{expense.description}</td>
-            <td>{expense.tag}</td>
-            <td>{expense.method}</td>
-            <td>{expense.value}</td>
-            <td>{expense.exchangeRates[expense.currency].name}</td>
+          <tr key={ expense.id }>
+            <td>{ expense.description }</td>
+            <td>{ expense.tag }</td>
+            <td>{ expense.method }</td>
+            <td>{ expense.value }</td>
+            <td>{ expense.exchangeRates[expense.currency].name }</td>
             <td>
-              {parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(
+              { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(
                 2
-              )}
+              ) }
             </td>
             <td>
-              {parseFloat(
+              { parseFloat(
                 expense.value * expense.exchangeRates[expense.currency].ask
               ).toFixed(2)}
             </td>
@@ -68,3 +68,4 @@ const mapStateToProps = ({ wallet: { expenses } }) => ({
 const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
+
