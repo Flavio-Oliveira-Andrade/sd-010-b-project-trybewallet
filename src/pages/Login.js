c5acf466e722e339d434811bc2aba60022f848f3
DennisMarcelo <dennismarcelo15@gmail.com>
2021-06-08 16:10:52 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="form-login">
+        <form>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="Digite seu senha"
+            data-testid="password-input"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
