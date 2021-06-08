5ec91f7eda1ff39c8d893c1c77aa79baafbd34b1
vinicius <viniciuspalenske@gmail.com>
2021-06-08 17:45:49 -0300

    [FEAT][Requisito 4] Renderiza Wallet page em "/carteira"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, Wallet } from './pages';
 
 function App() {
   return (
     <Switch>
+      <Route path="/carteira" component={ Wallet } />
       <Route path="/" component={ Login } />
     </Switch>
   );
