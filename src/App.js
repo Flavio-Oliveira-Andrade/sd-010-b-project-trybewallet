45a51b460239fe1ddef7e11086798427aa934004
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 17:25:44 -0300

    adicionando a rota "/wallet"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      {/* <Route path="/login" component={ Login } />
+      <Route path="/wallet" component={ Wallet } />
+      {/*
       <Route path="/register" component={ Register } />
       <Route path="/clients" component={ Clients } /> */}
     </Switch>
