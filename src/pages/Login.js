3b28ade8a1b1d78f94aa62134d424d34b4c3ba3d
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-06-08 15:22:36 -0300

    data-testid inseridos na pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,13 +9,13 @@ class Login extends React.Component {
         <label htmlFor="email">
           E-mail:
           {' '}
-          <input type="email" name="email" />
+          <input type="email" name="email" data-testid="email-input" />
         </label>
         {' '}
         <label htmlFor="password">
           Senha:
           {' '}
-          <input type="password" name="password" />
+          <input type="password" name="password" data-testid="password-input" />
         </label>
         {' '}
         <button type="button">Entrar</button>
