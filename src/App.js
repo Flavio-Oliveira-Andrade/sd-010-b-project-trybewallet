ab6985604c37b9f960ca6a886101d100c4e4746c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-08 17:49:46 -0300

    Remove o BrowserRouter do APP

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
+    <>
+      <h1>Trybe Wallet</h1>
       <Switch>
         <Route exact path="/" component={ Login } />
       </Switch>
-
-    </Router>
+    </>
   );
 }
 
