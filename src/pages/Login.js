94edf25a323a04dfd98b3f11f24a350b443328f6
Thiago Kachinsky <tjk091@gmail.com>
2021-06-08 14:47:00 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Logi</div>;
+    return (
+      <form>
+        <label htmlFor="input-email">
+          Email
+          <input type="text" data-testid="email-input" id="input-email" />
+        </label>
+
+        <label htmlFor="input-password">
+          Senha
+          <input type="password" data-testid="password-input" id="input-password" />
+        </label>
+
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
