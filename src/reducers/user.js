6a2137341ab9e2cb6e59e87321e7aa4ee6a36edf
Italo Matos <italomatos764@gmail.com>
2021-06-08 14:15:41 -0400

    Criando store

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const user = (state = INITIAL_USER_STATE, action) => {
+  switch (action.type) {
+  case 'SWITCH':
+    return {
+      ...state,
+      valor: 'valor',
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_USER_STATE, action) => {
+  switch (action.type) {
+  case 'SWITCH':
+    return {
+      ...state,
+      valor: 'valor',
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
