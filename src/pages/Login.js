9ca1a15c6d240e89dfb0a2e852de9ffafdb5cf98
Misael Bueno <misa.hbueno@gmail.com>
2021-06-08 17:49:34 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          Email
+          <input data-testid="email-input" type="email" />
+          Password
+          <input data-testid="password-input" type="password" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
