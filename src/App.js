ef241f49fc01efe9fd6ed5b8a5ddc6425703c7b0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 15:44:50 -0300

    adicionando a rota "/"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      {/* <Route path="/login" component={ Login } />
+      <Route path="/register" component={ Register } />
+      <Route path="/clients" component={ Clients } /> */}
+    </Switch>
+  );
 }
 
 export default App;
