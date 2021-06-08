27a4d5068f223a149d9f4e339e76b173f75182eb
vinicius <viniciuspalenske@gmail.com>
2021-06-08 16:59:59 -0300

    [FEAT][Requisito 1] Main page "/" =  Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    console.log(email, password);
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          type="text"
+          placeholder="e-mail"
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={ this.handleChange }
+        />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
