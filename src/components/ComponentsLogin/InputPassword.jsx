b8ecf5c527055026359add16c584e79e65107518
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-08 16:58:01 -0300

    Cria componente de input senha

---
+++ src/components/ComponentsLogin/InputPassword.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InputPassword() {
+  return (
+
+    <input
+      type="password"
+      data-testid="password-input"
+      placeholder="Insira sua senha"
+    />
+
+  );
+}
+
+export default InputPassword;
