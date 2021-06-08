1cc22e28401ecbf0fc6212ca787a08e18ffdc9bb
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-06-08 16:44:05 -0300

    Requirement 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet! Iniciando...</div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            <input type="text" name="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="password">
+            <input type="password" name="password" data-testid="password-input" />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
