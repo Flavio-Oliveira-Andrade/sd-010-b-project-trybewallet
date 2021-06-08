383eb6ee2f408afe357d5f521fc81554c709ad7a
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-08 17:04:40 -0300

    Import do componente Button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ButtonEnter from '../components/ComponentsLogin/ButtonEnter';
 import InputEmail from '../components/ComponentsLogin/InputEmail';
 import InputPassword from '../components/ComponentsLogin/InputPassword';
 
