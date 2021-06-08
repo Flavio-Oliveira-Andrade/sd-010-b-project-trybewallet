122944d8d68361ef2bd69728ae9354b4c7e84ac0
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-06-08 17:06:09 -0300

    primeiras partes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,58 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.habilitarButton = this.habilitarButton(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      button: false,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+    habilitarButton();
+  }
+
+  habilitarButton(){
+    const { email, password, button } = this.state;
+    const teste = email + password;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { handleChange } = this;
+    const { email, password, button } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ handleChange }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ handleChange }
+          />
+          <div>
+            <button type="button" disabled={ button }>Entrar</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
