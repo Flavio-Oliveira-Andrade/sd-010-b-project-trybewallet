8847451af295c3078ae6dd77e77dd93295df075b
Antenor <antenorzapata@gmail.com>
2021-06-08 17:50:40 -0300

    wallet reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,65 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  FETCH_CURRENCY,
+  DELETE_PAY,
+  UPDATE_PAY,
+  // FETCH_TRUE,
+  CURR_DATA,
+  AUTHORIZED_UPDATE,
+} from '../actions/types';
+
+const initialState = {
+  expenses: [],
+  currencies: [],
+  upDateItemId: 0,
+  bool: '',
+  // isFetching: false,
+};
+
+export default function wallet(state = initialState, action) {
+  switch (action.type) {
+  case FETCH_CURRENCY:
+    return {
+      ...state,
+      expenses: [
+        ...state.expenses,
+        { ...action.payload.args, id: state.expenses.length },
+      ],
+    };
+
+  case CURR_DATA:
+    return {
+      ...state,
+      currencies: Object.keys(action.payload.data),
+    };
+
+  case DELETE_PAY:
+    return {
+      ...state,
+      expenses: state.expenses.filter(
+        (item) => item.id !== action.payload.id,
+      ),
+    };
+
+  case UPDATE_PAY:
+    return {
+      ...state,
+      bool: action.payload.bool,
+      upDateItemId: action.payload.id,
+    };
+
+  case AUTHORIZED_UPDATE:
+    return {
+      ...state,
+      expenses: state.expenses.map((item) => (item.id === state.upDateItemId
+        ? {
+          ...item,
+          ...action.payload.args,
+        }
+        : item
+      )),
+      bool: '',
+    };
+  default:
+    return state;
+  }
+}
