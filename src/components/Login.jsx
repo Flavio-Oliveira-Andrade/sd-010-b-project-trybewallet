12d1a6dd144cb470e9deec155c50be3f6017af52
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-08 20:09:19 -0300

    Revert "Criando um arquivo para ser renderizado"
    This reverts commit 3e8e5189f9106d8a002e38f1fabaaa36b60f2a30.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,93 +1,23 @@
-import React from 'react';
-import loginUser from '../actions/index'
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-class Login extends React.Component {
-  constructor() {
-    super();
+// import React from 'react';
 
-    this.state = {
-      password: '',
-      inputLogin: '',
-      email: '',
-      action: true,
-    }
-  }
+// const handleChange = ({ target }) => {
+//   const input
+// }
 
-  validateEmail(mail) {
-    const format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    // Alessandra passou o Regex que ela utilizou no projeto dela.
-    if (mail.match(format)) {
-      return true;
-    }
-      return false;
-    }
-    
-    validateEmailAndPassword() {
-      const { inputLogin, password } = this.state;
-      const minLength = 6;
-      if (this.validateEmail(inputLogin) === true && password.length >= minLength) {
-       this.setState({
-          action: false,
-         });
-     } else {
-        this.setState({
-          action: true,
-        });
-      }
-    } 
-
-  handleClick = () => {
-    const { login } = this.props;
-    const input = document.getElementById('login').value;
-    login(input);
-  }
-  
-  handleChange = async ({ target }) => {
-    const button = document.getElementById('button');
-    const { name, value } = target;
-    this.setState({ [name]: value }, () => this.validateEmailAndPassword());
-  }
-
-  render () {
-    const { inputLogin, password, action } = this.state;
-    return (
-      <form>
-        <label htmlFor='login'>
-          Login
-          <input
-            value={ inputLogin }
-            name='inputLogin'
-            type='email'
-            id='login'
-            onChange={ (event) => this.handleChange(event) }
-            data-testid="email-input"
-          />
-        </label>
-        <label htmlFor='password'>
-          Password
-          <input
-            value={ password }
-            onChange={ (event) => this.handleChange(event) }
-            type='password'
-            name='password'
-            id='password'
-            data-testid="password-input"
-            minLength='6'
-          />
-        </label>
-        <button type='submit' id='button' onClick={ () => this.handleClick() } disabled={ action }>
-          <Link to='/carteira'>
-            Entrar
-          </Link>
-        </button>
-      </form>
-    )
-  }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  login: (mail) => dispatch(loginUser(mail)),
-})
-
-export default connect(null, mapDispatchToProps)(Login);
+// export default function Login() {
+//   return (
+//     <form>
+//       <label htmlFor='login'>
+//         Login
+//         <input type='email' id='login' onChange={ (event) =>  } data-testid="email-input" required />
+//       </label>
+//       <label htmlFor='password'>
+//         Password
+//         <input type='password' id='password' data-testid="password-input" minLength='6' required />
+//       </label>
+//       <button type='submit'>
+//         Entrar
+//       </button>
+//     </form>
+//   )
+// }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,22 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+// // Esse reducer será responsável por tratar as informações da pessoa usuária
 
-import { LOGIN_SUCCESS } from '../actions/index';
+// const INITIAL_STATE = {
+//   user: {
+//     email: '',
+//   },
+// }
 
-const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
-};
+// const user = (state = INITIAL_STATE, action) => {
+//   switch(action.type) {
+//     default:
+//       return state;
+//   }
+// }
 
-const user = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-  case (LOGIN_SUCCESS):
-    return (
-      { ...state, email: action.email }
-    );
-  default:
-    return state;
-  }
-};
-
-export default user;
+// export default user;
