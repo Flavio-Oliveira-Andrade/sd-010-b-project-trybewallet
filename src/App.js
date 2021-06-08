be3d10f43676c1c7b286e8f4b49b46dda4b314c0
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-06-08 15:43:58 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <Route path="/">
+        <Login />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,24 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'NEW ACTION':
+    return { ...state };
+  default:
+    return state;
+  }
+}
+
+export default reducer;
