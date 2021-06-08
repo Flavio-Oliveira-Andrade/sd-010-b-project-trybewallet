eb1ed8cb14946624015c87d15bce86ef8424c1aa
Daniel <darkdanielr@gmail.com>
2021-06-08 17:13:43 -0300

    1,2,3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,1 @@
-// Coloque aqui suas actions
+export const login = (value) => ({ type: 'LOGIN', value });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ store }>
+      <Switch>
+        <Route path="/" component={ App } />
+      </Switch>
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
