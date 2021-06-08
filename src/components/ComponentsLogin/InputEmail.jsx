0fa629347c95ac03e4733bb10dbaceb934501642
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-08 16:58:47 -0300

    Removendo label e adicionando placeholder

--- src/components/ComponentsLogin/InputEmail.jsx
+++ src/components/ComponentsLogin/InputEmail.jsx
@@ -1,18 +1,15 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 function InputEmail() {
   return (
-    <>
-      <label htmlFor="email-id">Insira o email:</label>
-      {' '}
 
-      <input
-        id="email-id"
-        type="text"
-        testid="email-input"
-      />
-    </>
+    <input
+      id="email-id"
+      type="text"
+      testid="email-input"
+      placeholder="Insira seu email"
+    />
+
   );
 }
 
