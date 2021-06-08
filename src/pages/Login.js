33e4bedca80e04426767d19e1528f5d28ae35106
Hugo Simoes <hugolfsimoes@gmail.com>
2021-06-08 15:35:58 -0300

    feito a primeira tarefa do projeto criando um formulario para login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        Login
+        <form>
+          <label htmlFor="email-input">
+            Email
+            <input id="email-input" data-testid="email-input" type="email" />
+          </label>
+          <label htmlFor="password-input">
+            Senha
+            <input id="password-input" data-testid="password-input" type="password" />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </section>
+    );
   }
 }
 
