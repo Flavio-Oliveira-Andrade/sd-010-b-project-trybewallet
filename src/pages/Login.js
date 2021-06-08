60b0c535abedeaff2c37956a7cba70c4bcffef9b
Thiago Kachinsky <tjk091@gmail.com>
2021-06-08 16:00:04 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,61 @@
 import React from 'react';
+// import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    // this.validarEmail = this.validarEmail.bind(this);
+
+    this.state = {
+      // email: '',
+      // buttonState: false
+    };
+  }
+
+  // validarEmail(e) {
+  //   const SIX = 6;
+  //   const emailRegex = /\S+@\S+\.\S+/;
+  //   e.preventDefault();
+  //   const email = e.target.email.value;
+  //   const password = e.target.password.value.length;
+  //   if (password.length >= SIX) {
+  //     this.setState({ buttonState: true });
+  //   }
+  //   if (email === emailRegex) {
+  //     this.setState({ buttonState: true});
+  //   }
+  // }
+
+  // redirect() {
+  //   return <Redirect from="/carteira" />;
+  // }
+
   render() {
     return (
-      <form>
-        <label htmlFor="input-email">
-          Email
-          <input type="text" data-testid="email-input" id="input-email" />
-        </label>
-
-        <label htmlFor="input-password">
-          Senha
-          <input type="password" data-testid="password-input" id="input-password" />
-        </label>
-
-        <button type="button">Entrar</button>
-      </form>
+      <div>
+        <form>
+          <label htmlFor="email">
+            E-mail:
+            <input
+              type="email"
+              id="email"
+              data-testid="email-input"
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              id="password"
+              data-testid="password-input"
+              name="password"
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     );
   }
 }
