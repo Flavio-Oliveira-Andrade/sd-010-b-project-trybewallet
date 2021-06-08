58fa8e1fce200b8a0afee7babfc5ba10c12e5f79
Flavio Andrade <foa12381@gmail.com>
2021-06-08 16:02:04 -0300

    add provider

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          E-mail:
+          <input type="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input type="password" id="password" maxLength="6" />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
