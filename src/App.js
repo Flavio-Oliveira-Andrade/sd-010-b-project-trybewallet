6742bf61563bf74bc61b5cee6b5c6b8dacc24719
Felippe <felippecorrea2010@gmail.com>
2021-06-08 15:18:48 -0300

    Requisito 1: Completando router adicionando o switich e adicionando button no componente login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Router, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return(
-    <Router exact path="/">{ Login }</Router>
+    <Switch>
+      <Router exact path="/">{ Login }</Router>
+    </Switch>
   );
 }
 
