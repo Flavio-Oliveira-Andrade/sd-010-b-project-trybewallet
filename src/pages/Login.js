ce10ffebc01600786e4aa23f7dd0a8b9817737e0
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-08 19:30:59 -0300

    ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input data-testid="email-input" type="email" required />
+        <input data-testid="password-input" type="password" required />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
