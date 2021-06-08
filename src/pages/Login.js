3ace7cb337e6aba5fae64804c89e4a8385e2d679
Miguel Dantas <miiguelalb@gmail.com>
2021-06-08 17:16:48 -0300

    Regex de Email e Password criados na pagina Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      invalidEmail: false,
+      invalidPassword: false,
+      buttonDisabled: true,
+      loggedIn: false,
+    };
+    this.validateFields = this.validateFields.bind(this);
+    this.setLogOn = this.setLogOn.bind(this);
+    this.loginEnabled = this.loginEnabled.bind(this);
+  }
+
+  setLogOn() {
+    this.setState({ loggedIn: true });
+  }
+
+  validateFields() {
+    const { email, password } = this.state;
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const regexPassword = /\d{6,}/;
+
+    if (email.match(regexEmail)) {
+      this.setState({ invalidEmail: false }, () => { this.loginEnabled(); });
+    } else {
+      this.setState({ invalidEmail: true }, () => { this.loginEnabled(); });
+    }
+
+    if (password.match(regexPassword)) {
+      this.setState({ invalidPassword: false }, () => { this.loginEnabled(); });
+    } else {
+      this.setState({ invalidPassword: true }, () => { this.loginEnabled(); });
+    }
+  }
+
+  loginEnabled() {
+    const { email, password, invalidEmail, invalidPassword } = this.state;
+    if (email === '' && password === '') {
+      return this.setState({ buttonDisabled: true });
+    }
+
+    this.setState({ buttonDisabled: invalidPassword || invalidEmail });
+  }
+
   render() {
+    const { buttonDisabled } = this.state;
+
     return (
       <div>
         <form>
@@ -11,6 +59,12 @@ class Login extends React.Component {
               data-testid="email-input"
               id="email-input"
               type="text"
+              name="usuario"
+              onChange={ (event) => {
+                this.validateFields();
+                this.setState({ email: event.target.value },
+                  () => { this.validateFields(); });
+              } }
             />
           </label>
           <label htmlFor="password-input">
@@ -19,11 +73,17 @@ class Login extends React.Component {
               data-testid="password-input"
               id="password-input"
               type="password"
+              onChange={ (event) => {
+                this.validateFields();
+                this.setState({ password: event.target.value },
+                  () => { this.validateFields(); });
+              } }
             />
           </label>
           <button
             type="button"
-            onClick={ () => {} }
+            onClick={ () => { this.setLogOn(); } }
+            disabled={ buttonDisabled }
           >
             Entrar
           </button>
