2ac8befcc41e8884b402fd222291464a22170386
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-06-08 15:44:45 -0300

    Adiciona algumas alteracoes iniciais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.renderRotes = this.renderRotes.bind(this);
+  }
+
+  renderRotes() {
+    return (
+      <Switch>
+        <Route path="/carteira" component={ Wallet } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    );
+  }
+
+  render() {
+    return <section>{this.renderRotes()}</section>;
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <input type="email" data-testid="email-input" placeholder="E-mail" />
+        <input type="password" data-testid="password-input" placeholder="Senha" />
+        <Link to="/carteira">
+          <button type="button">Entrar</button>
+        </Link>
+      </section>
+    );
   }
 }
 
