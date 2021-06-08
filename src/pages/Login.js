8d87ad9b4b884219fe2a47ce25ffd5ce266b1b0f
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-06-08 19:06:34 -0300

    1. Crie uma página inicial de login com os seguintes campos e características

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <div>Login</div>
+        <label htmlFor="email">
+          Email:
+          <input type="text" data-testid="email-input" id="email" />
+        </label>
+        <br />
+        <label htmlFor="password">
+          Password:
+          <input type="password" data-testid="password-input" id="password" />
+        </label>
+        <button type="button">Entrar</button>
+      </>
+    );
   }
 }
 
