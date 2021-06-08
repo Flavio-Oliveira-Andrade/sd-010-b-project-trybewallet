0093e4bdc58dab591fbc77028dd389c349fab0d1
Caio <thehaseol@gmail.com>
2021-06-08 19:45:21 -0300

    Delete Action criada

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const SET_EMAIL = 'SET_EMAIL';
 export const NEW_EXPENSE = 'NEW_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
 export const SET_EXCHANGE_RATES = 'SET_EXCHANGE_RATES';
 
 export const setEmail = (email) => ({
@@ -7,10 +8,14 @@ export const setEmail = (email) => ({
   email,
 });
 
-export const newExpense = (expenses, total) => ({
+export const newExpense = (expenses) => ({
   type: NEW_EXPENSE,
   expenses,
-  total,
+});
+
+export const deleteExpense = (id) => ({
+  type: DELETE_EXPENSE,
+  id,
 });
 
 export const setExchangeRates = (currencies, exchangeRates) => ({
