28004c4922e02687e1253fd9d50bd30820babc23
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-08 19:14:39 -0300

    correçao lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import saveUserInfo from '../actions';
 import PropTypes from 'prop-types';
+import saveUserInfo from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
