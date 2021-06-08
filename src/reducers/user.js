e4b28043f8d134412d97a95bda20d8b83e9cc40c
Thiago Leite <leite.tjs@gmail.com>
2021-06-08 16:46:04 -0300

    users and wallet reducers created

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const initialState = {};
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
