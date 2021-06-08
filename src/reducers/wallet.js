8bb07057a457709349d25fbcf4326c6c65072212
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-08 14:39:18 -0300

    Start

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const reducerWallet = (state,action) => {
+//   switch (key) {
+//     case value:
+
+//       break;
+
+//     default:
+//       break;
+//   }
+// }
