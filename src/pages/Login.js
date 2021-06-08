4f362fbbb84991c06ef6ec89da1efed98fa72562
Hugo Simoes <hugolfsimoes@gmail.com>
2021-06-08 18:32:58 -0300

    feito a tarefa 2 colocando validacao no email e na senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,65 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      isDisable: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.verificationValidInput = this.verificationValidInput.bind(this);
+  }
+
+  verificationValidInput() {
+    const { email, password } = this.state;
+    const minimalPasswordLength = 6;
+
+    // Verificação de email conseguido através do site do Stackoverflow no link https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    const emailValidationRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    if (emailValidationRegex.test(email) && password.length >= minimalPasswordLength) {
+      this.setState({ isDisable: false });
+    } else {
+      this.setState({ isDisable: true });
+    }
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.verificationValidInput());
+  }
+
   render() {
+    const { isDisable } = this.state;
     return (
       <section>
         Login
         <form>
           <label htmlFor="email-input">
             Email
-            <input id="email-input" data-testid="email-input" type="email" />
+            <input
+              name="email"
+              id="email-input"
+              data-testid="email-input"
+              type="email"
+              onChange={ this.handleChange }
+            />
           </label>
           <label htmlFor="password-input">
             Senha
-            <input id="password-input" data-testid="password-input" type="password" />
+            <input
+              name="password"
+              id="password-input"
+              data-testid="password-input"
+              type="password"
+              minLength="6"
+              onChange={ this.handleChange }
+            />
           </label>
-          <button type="button">Entrar</button>
+          <button type="submit" disabled={ isDisable }>Entrar</button>
         </form>
       </section>
     );
