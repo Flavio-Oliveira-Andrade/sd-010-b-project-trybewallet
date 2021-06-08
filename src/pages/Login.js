36a1f96006295010cccf9791527642dac3f42305
Roger Franco <rogerfrancogalt@gmail.com>
2021-06-08 18:08:02 -0400

    resolução do requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <label htmlFor="email">
+          Email:
+          <input data-testid="email-input" ide="email" type="text" name="name" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input data-testid="password-input" id="password" type="password" name="name" />
+        </label>
+        <button type="button">Entrar</button>
+      </>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,8 @@
+const INITIAL_STATE = {};
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+export default function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
