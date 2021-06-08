9dfb69fedf077773243b8095896ed0f344d61a11
Junior030 <juniorhenrique030@gmail.com>
2021-06-08 15:09:54 -0300

    Initial Project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,14 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_USER = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+export default INITIAL_USER;
