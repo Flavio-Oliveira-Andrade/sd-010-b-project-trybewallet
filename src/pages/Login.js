26b4977306bc5997fb1853a3312af8974197c8a9
Thiago Kachinsky <tjk091@gmail.com>
2021-06-08 18:31:22 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,51 @@
 import React from 'react';
-// import { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor() {
     super();
 
-    // this.validarEmail = this.validarEmail.bind(this);
+    this.validarEmail = this.validarEmail.bind(this);
+    this.emailInput = this.emailInput.bind(this);
+    this.passwordInput = this.passwordInput.bind(this);
+    this.redirect = this.redirect.bind(this);
 
     this.state = {
-      // email: '',
-      // buttonState: false
+      email: '',
+      password: '',
+      buttonState: false,
+      redirect: false,
     };
   }
 
-  // validarEmail(e) {
-  //   const SIX = 6;
-  //   const emailRegex = /\S+@\S+\.\S+/;
-  //   e.preventDefault();
-  //   const email = e.target.email.value;
-  //   const password = e.target.password.value.length;
-  //   if (password.length >= SIX) {
-  //     this.setState({ buttonState: true });
-  //   }
-  //   if (email === emailRegex) {
-  //     this.setState({ buttonState: true});
-  //   }
-  // }
+  emailInput(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
 
-  // redirect() {
-  //   return <Redirect from="/carteira" />;
-  // }
+  passwordInput(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  validarEmail() {
+    const { password, email } = this.state;
+    const SIX = 6;
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    if (password.length >= SIX && emailRegex.test(email)) {
+      this.setState({ buttonState: true, redirect: true });
+    }
+  }
+
+  redirect() {
+    return <Redirect to="/teste" />;
+  }
 
   render() {
+    const { buttonState, redirect } = this.state;
     return (
       <div>
         <form>
@@ -42,6 +56,8 @@ class Login extends React.Component {
               id="email"
               data-testid="email-input"
               name="email"
+              onChange={ this.emailInput }
+              onKeyUp={ this.validarEmail }
             />
           </label>
           <label htmlFor="password">
@@ -51,9 +67,14 @@ class Login extends React.Component {
               id="password"
               data-testid="password-input"
               name="password"
+              onChange={ this.passwordInput }
+              onKeyUp={ this.validarEmail }
             />
           </label>
-          <button type="submit">Entrar</button>
+
+          {buttonState === false
+            ? <button type="button" disabled>Entrar</button>
+            : <button type="button" onClick={ redirect && this.redirect }>Entrar</button>}
         </form>
       </div>
     );
