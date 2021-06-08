1a434b2b482d3112c679641c899b4d1839abd378
Caio <thehaseol@gmail.com>
2021-06-08 19:19:45 -0300

    Componente para renderizar dados descriçao, valor e etc

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ export const NEW_EXPENSE = 'NEW_EXPENSE';
 export const SET_EXCHANGE_RATES = 'SET_EXCHANGE_RATES';
 
 export const setEmail = (email) => ({
-  tipo: SET_EMAIL,
+  type: SET_EMAIL,
   email,
 });
 
@@ -21,15 +21,11 @@ export const setExchangeRates = (currencies, exchangeRates) => ({
 
 export const getExchangeRates = () => async (dispatch) => {
   const currencyAPI = 'https://economia.awesomeapi.com.br/json/all';
-  const realBrasileiro = -16;
   const exchangeRates = await fetch(currencyAPI)
     .then((response) => response.json())
     .then((json) => json);
   const currencies = Object.keys(exchangeRates)
     .filter((coin) => coin !== 'USDT')
     .map((coin) => coin);
-  Object.keys(exchangeRates).forEach((coin) => {
-    exchangeRates[coin].name = exchangeRates[coin].name.slice(0, realBrasileiro);
-  });
   dispatch(setExchangeRates(currencies, exchangeRates));
 };
