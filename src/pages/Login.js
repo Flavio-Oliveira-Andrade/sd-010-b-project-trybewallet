8e2ca2693d6db55f1699ad58387d34331e61fba4
Miguel Dantas <miiguelalb@gmail.com>
2021-06-08 16:40:13 -0300

    Primeiro requisito concluído

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            Usuário:
+            <input
+              data-testid="email-input"
+              id="email-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              data-testid="password-input"
+              id="password-input"
+              type="password"
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ () => {} }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
