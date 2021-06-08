16866d18b64138081396aa15dff6595fce0a48fe
Felippe <felippecorrea2010@gmail.com>
2021-06-08 14:48:11 -0300

    Adicionando requisito 1: criando store e provider

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+
+const rootReducers = combineReducers({
+  reducer,
+})
+
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
