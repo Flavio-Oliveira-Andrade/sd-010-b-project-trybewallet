9c1e642a48fcd2151e668c99c951a988ebb71a61
bidaffonso <bidaffondo@gmail.com>
2021-06-08 19:34:17 -0400

    Requisito 1 (Pagina de Login com Email, senha e botao 'Entrar')

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="email-input">
+            Email:
+            <input data-testid="email-input" type="text" />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="password-input">
+            Senha:
+            <input data-testid="password-input" type="password" />
+          </label>
+          <br />
+          <br />
+          <button type="submit" name="entrar">Entrar</button>
+        </form>
+
+      </div>
+    );
   }
 }
 
