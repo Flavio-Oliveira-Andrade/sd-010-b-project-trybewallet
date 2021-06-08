09899ecf843390f5d1bbf4cd7640b635c876ce1b
riquee <strikedv@hotmail.com>
2021-06-08 14:14:46 -0300

    Requisito 1,2 e 3 finalizados

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_LOGIN } from '../actions';
+
+const INITIAL_LOGIN = { email: '' };
+
+export default (state = INITIAL_LOGIN, action) => {
+  switch (action.type) {
+  case SET_LOGIN:
+    return { ...state, email: action.email };
+  default:
+    return state;
+  }
+};
