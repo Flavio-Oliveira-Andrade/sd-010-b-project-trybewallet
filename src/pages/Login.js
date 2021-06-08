83b357c9fe63d747dcdd6d822f5a1af7a911ad72
Junior030 <juniorhenrique030@gmail.com>
2021-06-08 15:29:36 -0300

    Pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            E-mail:
+            <input type="email" id="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input type="password" id="password" data-testid="password-input" />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
