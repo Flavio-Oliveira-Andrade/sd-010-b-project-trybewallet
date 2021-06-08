475c04f685482af1eeda4f750c428567f2ff13fe
Mariana Mohr <marianamohr@gmail.com>
2021-06-08 15:26:56 -0300

    Requisito 1 e redux criados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import './App.css';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route to="/" component={ Login } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main className="login">
+        Login
+        <form>
+          <label htmlFor="email">
+            Email
+            <input id="email" type="email" data-testid="email-input" />
+          </label>
+          <label htmlFor="senha">
+            Senha:
+            <input id="senha" type="password" data-testid="password-input" />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </main>);
   }
 }
 
