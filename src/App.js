33138b636d520fdf82d150ed19d42bc78c8ba505
Joao Vanelli <vanellijoao@gmail.com>
2021-06-08 16:03:07 -0300

    Adding some style hihi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
+import './App.css';
+
 import Login from './pages/Login';
 
 function App() {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,29 +14,34 @@ class Login extends React.Component {
     const { email, password } = this.state;
 
     return (
-      <div className="login">
-        <section className="inputs">
-          <input
-            type="text"
-            onChange={ (e) => this.setState({ email: e.target.value }) }
-            placeholder="email"
-            data-testid="email-input"
-          />
-          <input
-            type="password"
-            onChange={ (e) => this.setState({ password: e.target.value }) }
-            placeholder="senha"
-            data-testid="password-input"
-          />
-        </section>
-        <div className="link">
-          <Link
-            to="/clients"
-            onClick={ () => console.log({ email, password }) }
-            data-testid="btn-login"
-          >
-            Entrar
-          </Link>
+      <div className="box">
+        <div className="login">
+          <div className="img">
+            <img src="" alt="" />
+          </div>
+          <section className="inputs">
+            <input
+              type="text"
+              onChange={ (e) => this.setState({ email: e.target.value }) }
+              placeholder="email"
+              data-testid="email-input"
+            />
+            <input
+              type="password"
+              onChange={ (e) => this.setState({ password: e.target.value }) }
+              placeholder="senha"
+              data-testid="password-input"
+            />
+          </section>
+          <div className="link">
+            <Link
+              to="/clients"
+              onClick={ () => console.log({ email, password }) }
+              data-testid="btn-login"
+            >
+              Entrar
+            </Link>
+          </div>
         </div>
       </div>
     );
