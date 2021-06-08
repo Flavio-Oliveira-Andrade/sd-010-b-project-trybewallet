6c41d7e7933165d470b2fdf6092147e2c19c9a2c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 19:33:12 -0300

    requisito 5 - exibindo o email, a despesa total e o câmbio no header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.renderHeader = this.renderHeader.bind(this);
+  }
+
+  renderHeader() {
+    const { email/* , currencies, expenses */ } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        {/* <span data-testid="total-field">{ `R$ ${currencies - expenses}` }</span> */}
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+        { this.renderHeader() }
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = ({ user: { email }/* ,wallet: { currencies, expenses } */ }) => ({
+  email,
+  /* currencies,
+  expenses, */
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
