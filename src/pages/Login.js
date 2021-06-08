dabf57a82c2b46bc018cb9b6c2242dee4e169972
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-06-08 15:40:42 -0300

    Email & password validation on login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailVerified: false,
+      passwordVerified: false,
+    };
+    this.verifyEmail = this.verifyEmail.bind(this);
+    this.verifyPassword = this.verifyPassword.bind(this);
+  }
+
+  verifyEmail({ target }) {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    this.setState({ emailVerified: emailRegex.test(target.value) });
+  }
+
+  verifyPassword({ target }) {
+    const passwordMinLength = 6;
+
+    this.setState({
+      passwordVerified: target.value.length >= passwordMinLength,
+    });
+  }
+
   render() {
+    const { emailVerified, passwordVerified } = this.state;
     return (
       <div>
         <form action="">
@@ -11,6 +37,7 @@ class Login extends React.Component {
               type="email"
               name="userEmail"
               data-testid="email-input"
+              onChange={ this.verifyEmail }
             />
           </label>
           <label htmlFor="userPassword">
@@ -19,10 +46,13 @@ class Login extends React.Component {
               type="password"
               name="userPassword"
               data-testid="password-input"
+              onChange={ this.verifyPassword }
             />
           </label>
         </form>
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ !emailVerified || !passwordVerified }>
+          Entrar
+        </button>
       </div>
     );
   }
