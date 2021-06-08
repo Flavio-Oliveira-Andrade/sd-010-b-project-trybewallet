fd576f72bb2e5b8f75cdd116b39435c60006ee00
Flavio Andrade <foa12381@gmail.com>
2021-06-08 20:04:49 -0300

    valida forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const emailValido = email.match(/^[^ ]+@[^ ]+.[a-z]{2,3}$/);
     return (
-      <div>
+      <form>
         <label htmlFor="email">
           E-mail:
-          <input type="email" id="email" />
+          <input type="email" id="email" onChange={ this.handleChange } />
         </label>
         <label htmlFor="password">
           Senha:
-          <input type="password" id="password" maxLength="6" />
+          <input
+            type="password"
+            id="password"
+            maxLength="12"
+            onChange={ this.handleChange }
+          />
         </label>
-        <button type="button">Entrar</button>
-      </div>
+        {/* {(email) ? button.on : button.offline } */}
+        <button
+          type="button"
+          disabled={ !emailValido || password.length <= '5' }
+        >
+          Entrar
+
+        </button>
+      </form>
     );
   }
 }
-
+// creditto a Antenor Zapata;
 export default Login;
