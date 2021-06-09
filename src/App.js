82eda3d12bd9d12b0292b2dbb696e2cd9e3b2450
Renan0211 <renan.antunes.rocha@gmail.com>
2021-06-09 18:40:33 -0300

    Implementa um cabeçalho para a pagina carteira

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Trybe Wallet</h1>
       </header>
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route exact path="/" component={ Login } />
       </Switch>
     </div>
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.calculateTotal = this.calculateTotal.bind(this);
+  }
+
+  calculateTotal() {
+    const { userExpenses } = this.props;
+    const values = userExpenses.map((expense) => expense.value);
+    const total = values.reduce((acc, curr) => acc + curr, 0);
+    if (!total) return 0;
+    return total;
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    const total = this.calculateTotal();
+    return (
+      <header>
+        <section data-testid="email-field">{userEmail}</section>
+        <section data-testid="total-field">{`Total: ${total}`}</section>
+        <section data-testid="header-currency-field">Moeda atual: BRL</section>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+  userExpenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+  userExpenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
