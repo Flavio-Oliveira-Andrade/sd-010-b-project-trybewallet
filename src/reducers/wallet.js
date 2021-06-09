ea560a91f0030c1503b816e0ae9dbb06d427d905
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-09 10:55:17 -0300

    implementando o reducer wallet, salvando as siglas das moedas no redux

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,12 +1,20 @@
+import { SAVE_CURRENCIES } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
 };
 
-function wallet(state = INITIAL_STATE, { type }) {
+function wallet(state = INITIAL_STATE, { type, currencies }) {
   switch (type) {
-  case '':
-    return '';
+  case SAVE_CURRENCIES:
+    return {
+      ...state,
+      currencies: [
+        ...state.currencies,
+        ...currencies,
+      ],
+    };
   default:
     return state;
   }
