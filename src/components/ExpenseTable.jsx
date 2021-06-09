87817b0473ab2ec054f0744390ef1141d867a670
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-09 20:51:06 -0300

    adicionando função que renderiza o cabeçalho de tabela

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 function ExpenseTable() {
-  const tableHead = () => (
-    <thead>
-      <tr>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
-      </tr>
-    </thead>
-  );
+const tableHead = () => (
+  <thead>
+    <tr>
+      <th>Descrição</th>
+      <th>Tag</th>
+      <th>Método de pagamento</th>
+      <th>Valor</th>
+      <th>Moeda</th>
+      <th>Câmbio utilizado</th>
+      <th>Valor convertido</th>
+      <th>Moeda de conversão</th>
+      <th>Editar/Excluir</th>
+    </tr>
+  </thead>
+);
 
   return (
     <table>
