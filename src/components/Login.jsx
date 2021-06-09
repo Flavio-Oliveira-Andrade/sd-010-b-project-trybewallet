bd16a2d2da66855eb531a203c127b4d79916908a
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-09 15:23:48 -0300

    Passando nos 3 testes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import loginUser from '../actions';
 
 class Login extends React.Component {
   constructor() {
     super();
 
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmailAndPassword = this.validateEmailAndPassword.bind(this);
+
     this.state = {
       login: '',
+      disable: true,
       password: '',
-      email: '',
-    };
-
-    handleChange = ({ target: { name, value } }) => {
-      this.setState({ [name]: value });
     };
   }
 
+  inviteState() {
+    const { login } = this.state;
+    const { add } = this.props;
+
+    add(login);
+  }
+
+  validateEmail(mail) {
+    const format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    // Alessandra passou o Regex que ela utilizou no projeto dela.
+    if (mail.match(format)) {
+      return true;
+    }
+    return false;
+  }
+
+  validateEmailAndPassword() {
+    const { login, password } = this.state;
+    const minLength = 6;
+    if (this.validateEmail(login) === true && password.length >= minLength) {
+      this.setState({
+        disable: false,
+      });
+    } else {
+      this.setState({
+        disable: true,
+      });
+    }
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => this.validateEmailAndPassword());
+  }
+
   render() {
-    const { login, password, email } = this.state;
+    const { login, password, disable } = this.state;
     return (
       <form>
         <label htmlFor="login">
@@ -27,7 +62,7 @@ class Login extends React.Component {
             name="login"
             value={ login }
             data-testid="email-input"
-            onChange={ (event) => handleChange(event) }
+            onChange={ (event) => this.handleChange(event) }
           />
         </label>
         <label htmlFor="password">
@@ -38,10 +73,10 @@ class Login extends React.Component {
             name="password"
             value={ password }
             data-testid="password-input"
-            onChange={ (event) => handleChange(event) }
+            onChange={ (event) => this.handleChange(event) }
           />
         </label>
-        <button type="submit">
+        <button type="submit" disabled={ disable } onClick={ this.inviteState }>
           Entrar
         </button>
       </form>
@@ -49,4 +84,8 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = () => ({
+  add: (mail) => dispatch(loginUser(mail)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
