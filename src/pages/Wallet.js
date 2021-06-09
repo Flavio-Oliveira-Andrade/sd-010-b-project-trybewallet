988e45cd35ca1c845d302936ea596466648fbc2c
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-09 11:33:05 -0300

    resolvido problema de propTypes

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <h4 data-testid="email-field">{userEmail}</h4>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+export default connect(mapStateToProps)(Wallet);
