8fe579e1dd807bcff2356a154cdd10ed698924a6
Johnny-batata <you@example.com>
2021-06-09 18:08:39 -0300

    fix lint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
 // Coloque aqui suas actions
-const login = (value) => ({ type: 'LOGIN', value });
+export const login = (value) => ({ type: 'LOGIN', value });
 
-export default login;
+export const batata = (value) => ({ type: 'BATATA', value });
