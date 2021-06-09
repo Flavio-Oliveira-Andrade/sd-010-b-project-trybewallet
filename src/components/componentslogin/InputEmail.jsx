dab55648bcbbc51d289fbd920661bb0fbd30ccb8
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-09 12:23:05 -0300

    Cria as props do input

--- src/components/componentslogin/InputEmail.jsx
+++ src/components/componentslogin/InputEmail.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-function InputEmail() {
+function InputEmail({
+  id = 'id_do_input_email',
+  placeholder = '',
+  inputValue = '',
+  onInputChange = null,
+}) {
+  function handleInputChange({ currentTarget }) {
+    const newValue = currentTarget.value;
+    if (onInputChange) {
+      onInputChange(newValue);
+    }
+  }
   return (
 
     <input
-      id="email-id"
+      id={ id }
+      value={ inputValue }
       type="text"
       data-testid="email-input"
-      placeholder="Insira seu email"
+      placeholder={ placeholder }
+      onChange={ handleInputChange }
     />
-
   );
 }
 
