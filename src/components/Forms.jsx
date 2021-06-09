2995ac1e19995e41ce007a81d94b732f521bf21d
Daniel <dsouzaborini@gmail.com>
2021-06-09 16:47:25 -0300

    add requisito 6

---
+++ src/components/Forms.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="name">
+          Valor
+          <input type="text" name="name" />
+        </label>
+        <label htmlFor="desc">
+          Descrição
+          <input type="text" name="desc" />
+        </label>
+        <label htmlFor="moeda">
+          Moeda
+          <select>
+            <option value="valor1">Valor 1</option>
+          </select>
+        </label>
+        <label htmlFor="desc">
+          Método de pagamento
+          <select>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="desc">
+          Tag
+          <select>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default Forms;
