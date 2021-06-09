6812af49cd3510ef06d7a2e7ab6d2e9341021765
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-06-09 17:34:50 -0300

    Requirement 5

---
+++ src/pages/HeaderWallet.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HeaderWallet extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
+    );
+  }
+}
+
+export default HeaderWallet;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import HeaderWallet from './HeaderWallet';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <HeaderWallet email={ email } />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
