bb174c429bdc049a81ce1250922706bffa43bc7c
Misael Bueno <misa.hbueno@gmail.com>
2021-06-09 19:20:25 -0300

    requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,14 +6,50 @@ class Wallet extends React.Component {
   render() {
     const { emailUser } = this.props;
     return (
-      <header>
-        <div data-testid="email-field">
-          Email:
-          {emailUser}
-        </div>
-        <span data-testid="total-field">Despesa Total: R$ 0 </span>
-        <span data-testid="header-currency-field">BRL</span>
-      </header>
+      <div>
+        <header>
+          <div data-testid="email-field">
+            Email:
+            {emailUser}
+          </div>
+          <span data-testid="total-field">Despesa Total: R$ 0 </span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <form>
+          <label htmlFor="number">
+            Valor
+            <input type="number" id="number" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" id="description" />
+          </label>
+          <label htmlFor="coin">
+            Moeda
+            <select id="coin">
+              <option value="teste">teste</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select id="payment">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="credito">Cartão de crédito</option>
+              <option value="debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
