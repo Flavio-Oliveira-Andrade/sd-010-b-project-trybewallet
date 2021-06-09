fb31a72959378f2544bdaebec82b73e854779837
Daniel Ceci <danielceci96@gmail.com>
2021-06-09 00:58:55 -0300

    renomeando a rota e ver se agora da certo o REQ. 4

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Switch>
       <Route path="/" component={ login } />
-      <Route path="carteira" component={ Wallet } />
+      <Route path="/carteira" component={ Wallet } />
     </Switch>
   ); // o componente Wallet é o dentro de pages, com W maiúsculo. Não é o dentro de reducers com w minúsculo.
 }
