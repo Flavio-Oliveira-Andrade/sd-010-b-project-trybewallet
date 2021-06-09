0084374bdeb599ff934251034c49d754f1b362c0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-09 12:24:18 -0300

    Cria o state dos inputs e adiciona as props criadas nos componentes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonEnter from '../components/componentslogin/ButtonEnter';
 import InputEmail from '../components/componentslogin/InputEmail';
 import InputPassword from '../components/componentslogin/InputPassword';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(newEmail) {
+    setEmail(newEmail);
+  }
+
+  function handlePasswordChange(newPassword) {
+    setPassword(newPassword);
+  }
+
+  function clickEnter() {
+    console.log('clicou');
+  }
   return (
     <>
-      <InputEmail />
-      <InputPassword />
-      <ButtonEnter />
+      <InputEmail
+        placeholder="Insira seu email"
+        inputValue={ email }
+        onInputChange={ handleEmailChange }
+      />
+      <InputPassword
+        placeholder="Insira sua senha"
+        inputValuePassword={ password }
+        onInputPasswordChange={ handlePasswordChange }
+      />
+      <ButtonEnter enterLogin={ clickEnter } />
     </>
   );
 }
