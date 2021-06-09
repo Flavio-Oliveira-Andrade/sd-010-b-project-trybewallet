0dad82d16c81ea3a1d7baf6e307bae0cceb91be1
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-06-09 16:12:10 -0300

    Requirement 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/carteira">
+            <Wallet />
           </Route>
         </Switch>
       </div>
