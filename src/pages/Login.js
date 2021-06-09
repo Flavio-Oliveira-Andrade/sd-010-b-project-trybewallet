ea98006b88ad2243396e48265b8efdd53f3263b8
Vinicius <vini_bodra@hotmail.com>
2021-06-09 15:05:00 -0300

    Corrigindo linter

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import validateEmail from '../services';
-import { redirectCarteira } from '../actions'
 import { Redirect } from 'react-router-dom';
+import { validateEmail } from '../services';
+import { redirectCarteira } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
@@ -14,57 +14,75 @@ class Login extends React.Component {
       input: '',
       pass: '',
       shouldRedirect: false,
-    }
+    };
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePassChange = this.handlePassChange.bind(this);
     this.handleRedirectCarteira = this.handleRedirectCarteira.bind(this);
   }
 
-  handlePassChange (e) {
+  handlePassChange(e) {
+    const { pass } = this.state;
+    const extensaoSenha = 5;
     this.setState({
       pass: e.target.value,
     },
-    () => {if(this.state.pass.length > 5) this.setState({
-      invalidPassword: false,
-    })})
-    }
+    () => {
+      if (pass.length > extensaoSenha) {
+        this.setState({
+          invalidPassword: false,
+        });
+      }
+    });
+  }
 
-  handleEmailChange (e) {
+  handleEmailChange(e) {
     this.setState({
       input: e.target.value,
-    })
+    });
 
-    if(validateEmail(e.target.value)) {
+    if (validateEmail(e.target.value)) {
       this.setState({
         invalidEmail: false,
-      })
+      });
     }
   }
 
-  handleRedirectCarteira () {
-    this.props.redirectCarteira(this.state.input);
+  handleRedirectCarteira() {
+    const { input } = this.state;
+    redirectCarteira(input);
     this.setState({
       shouldRedirect: true,
-    })
+    });
   }
-  
+
   render() {
     const { invalidEmail, invalidPassword, input, shouldRedirect } = this.state;
     const valid = invalidEmail || invalidPassword;
-    
-    const componente = <div>
-    <input 
-      data-testid="email-input"
-      type='email'
-      onChange={this.handleEmailChange}
-      value={input} 
-    />
-    <input data-testid="password-input" type='password' onChange={this.handlePassChange}/>
-    <button type='button' disabled={valid} onClick={this.handleRedirectCarteira} >Entrar </button>
-  </div>
-    
-    const conditionalRender = shouldRedirect ? <Redirect to="/carteira"/> : componente;
+
+    const componente = (
+      <div>
+        <input
+          data-testid="email-input"
+          type="email"
+          onChange={ this.handleEmailChange }
+          value={ input }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          onChange={ this.handlePassChange }
+        />
+        <button
+          type="button"
+          disabled={ valid }
+          onClick={ this.handleRedirectCarteira }
+        >
+          Entrar
+        </button>
+      </div>);
+
+    const conditionalRender = shouldRedirect ? <Redirect to="/carteira" /> : componente;
     return (
       conditionalRender
     );
