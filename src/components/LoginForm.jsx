338e85ca03e85951b9c02626890ea68f4dc2ed80
Alan Tanaka <alanctnk@gmail.com>
2021-06-08 21:42:51 -0300

    Requisito 2 ok?

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldRedirect: false,
+      disabled: true,
+    };
+    this.validateLogin = this.validateLogin.bind(this);
+    this.walletRedirect = this.walletRedirect.bind(this);
+  }
+
+  walletRedirect(e) {
+    e.preventDefault();
+    this.setState((oldState) => ({ ...oldState, shouldRedirect: true }));
+  }
+
+  validateLogin() {
+    const emailInput = document.getElementById('email-input').value;
+    const passwordInput = document.getElementById('password-input').value;
+    const SIX_CHAR = 6;
+    // Regex para email https://regexr.com/3e48o
+    const validEmail = (/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/).test(emailInput);
+    const validPassword = passwordInput.length >= SIX_CHAR;
+    if (validEmail && validPassword) {
+      // document.getElementById('formButton').disabled = false;
+      this.setState((oldState) => ({ ...oldState, disabled: false }));
+    } else {
+      // document.getElementById('formButton').disabled = true;
+      this.setState((oldState) => ({ ...oldState, disabled: true }));
+    }
+  }
+
   render() {
+    const { shouldRedirect, disabled } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/carteira" />;
+    }
     return (
       <form>
         <h2>LOGIN</h2>
         <fieldset>
           <label htmlFor="email-input">
             Email:
-            <input type="text" id="email-input" data-testid="email-input" />
+            <input
+              type="text"
+              onChange={ this.validateLogin }
+              id="email-input"
+              data-testid="email-input"
+              // value="teste@email.com"
+            />
           </label>
           <br />
           <label htmlFor="password-input">
             Senha:
-            <input type="text" id="password-input" data-testid="password-input" />
+            <input
+              type="password"
+              onChange={ this.validateLogin }
+              id="password-input"
+              data-testid="password-input"
+            />
           </label>
           <br />
-          <button type="submit">Entrar</button>
+          <button
+            id="formButton"
+            type="submit"
+            onClick={ this.walletRedirect }
+            disabled={ disabled }
+          >
+            Entrar
+
+          </button>
         </fieldset>
       </form>
     );
