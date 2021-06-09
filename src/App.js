42cf4002f99b2e1e78e4b2ba7afcdfd27f17481f
Daniel Ceci <danielceci96@gmail.com>
2021-06-09 01:27:28 -0300

    Refatorando o req 4- O erro era nao ter usado o exact antes do Path

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import Wallet from './pages/Wallet';
 function App() {
   return (
     <Switch>
-      <Route path="/" component={ login } />
-      <Route path="/carteira" component={ Wallet } />
+      <Route exact path="/" component={ login } />
+      <Route exact path="/carteira" component={ Wallet } />
     </Switch>
   ); // o componente Wallet é o dentro de pages, com W maiúsculo. Não é o dentro de reducers com w minúsculo.
+  // perdi 1 hora no REQ 4 porque não tinha colocado o exact antes do path. Era isso que não tava fazendo a rota dar certo quando colocava /carteira
 }
 
 export default App;
