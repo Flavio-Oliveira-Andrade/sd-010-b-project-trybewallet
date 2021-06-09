494827e0f4aba7a374243cd2425e650c28da48e1
Camila Arruda <camilaam1802@gmail.com>
2021-06-09 15:12:39 -0300

    Requisito 2, feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { login } from '../actions';
 
 class Login extends React.Component {
   constructor(props) {
@@ -6,12 +10,28 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      disabledButton: true,
     };
+    this.validarCampos = this.validarCampos.bind(this);
+  }
+
+  validarCampos() {
+    const { email, password } = this.state;
+    const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const validPassword = /\d{5,}/;
+
+    if (email.match(validEmail)) {
+      if (password.match(validPassword)) {
+        this.setState({ disabledButton: false });
+      }
+    } else {
+      this.setState({ disabledButton: true });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
-    console.log(email, password);
+    const { email, password, disabledButton } = this.state;
+    const { login } = this.props;
     return (
       <main>
         <h1>Login</h1>
@@ -22,7 +42,10 @@ class Login extends React.Component {
               data-testid="email-input"
               type="email"
               name="email"
-              onChange={ (event) => this.setState({ email: event.target.value }) }
+              onChange={ (event) => {
+                this.validarCampos();
+                this.setState({ email: event.target.value });
+              } }
             />
           </label>
           <label htmlFor="InputPassword">
@@ -31,14 +54,26 @@ class Login extends React.Component {
               data-testid="password-input"
               type="password"
               name="password"
-              onChange={ (event) => this.setState({ password: event.target.value }) }
+              onChange={ (event) => {
+                this.validarCampos();
+                this.setState({ password: event.target.value });
+              } }
             />
           </label>
-          <button type="submit">Entrar</button>
+          <Link to="/carteira" onClick={ () => login({ email, password }) }>
+            <button type="button" disabled={ disabledButton }>Entrar</button>
+          </Link>
         </form>
       </main>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (e) => dispatch(login(e)) });
+
+export default connect(null, mapDispatchToProps)(Login);
