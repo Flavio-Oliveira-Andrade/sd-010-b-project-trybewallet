a8b32b6d3a607a09fd671f3395b27bc41c4367ee
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-08 23:37:46 -0300

    requisito 6 - adicionando o formulário para cadastrar despesas

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,6 +6,11 @@ class Wallet extends React.Component {
   constructor() {
     super();
     this.renderHeader = this.renderHeader.bind(this);
+    this.renderValueLabel = this.renderValueLabel.bind(this);
+    this.renderDescriptionLabel = this.renderDescriptionLabel.bind(this);
+    this.renderCurrencySelect = this.renderCurrencySelect.bind(this);
+    this.renderPaymentMethodSelect = this.renderPaymentMethodSelect.bind(this);
+    this.renderExpenseCategorySelect = this.renderExpenseCategorySelect.bind(this);
   }
 
   renderHeader() {
@@ -20,10 +25,74 @@ class Wallet extends React.Component {
     );
   }
 
+  renderValueLabel() {
+    return (
+      <label htmlFor="valor">
+        Valor:
+        <input type="text" name="valor" id="valor" />
+      </label>
+    );
+  }
+
+  renderDescriptionLabel() {
+    return (
+      <label htmlFor="descricao">
+        Descrição:
+        <input type="text" name="descrição" id="descricao" />
+      </label>
+    );
+  }
+
+  renderCurrencySelect() {
+    return (
+      <label htmlFor="moeda">
+        Moeda:
+        <select id="moeda" name="moeda">
+          <option value=""> </option>
+        </select>
+      </label>
+    );
+  }
+
+  renderPaymentMethodSelect() {
+    return (
+      <label htmlFor="paymentMethod">
+        Método de pagamento:
+        <select id="paymentMethod" name="metodoDePagamento">
+          <option value="dinheiro">Dinheiro</option>
+          <option value="credito">Cartão de crédito</option>
+          <option value="debito">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderExpenseCategorySelect() {
+    return (
+      <label htmlFor="tag">
+        Tag:
+        <select id="tag" name="tag">
+          <option value="alimentacao">Alimentação</option>
+          <option value="lazer">Lazer</option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <>
         { this.renderHeader() }
+        <form>
+          { this.renderValueLabel() }
+          { this.renderDescriptionLabel() }
+          { this.renderCurrencySelect() }
+          { this.renderPaymentMethodSelect() }
+          { this.renderExpenseCategorySelect() }
+        </form>
       </>
     );
   }
