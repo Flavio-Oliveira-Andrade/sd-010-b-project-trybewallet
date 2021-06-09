59aa583f89e0fa38d129817a0b060d22bb0cab25
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-06-09 18:38:48 -0300

    Correçao linter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={ (props) => <Login { ...props } />} />
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
         <Route path="/carteira" render={ () => <Wallet /> } />
       </Switch>
     );
