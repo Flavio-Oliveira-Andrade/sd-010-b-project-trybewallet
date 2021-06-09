459e83eacb39ea3d784302118a2611a61f2867f7
Mariana Savoldi <marianasavodi123@gmail.com>
2021-06-09 20:08:45 -0300

    Requisito 1 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="App">
+        <header>
+          <h2>Hello, TrybeWallet!</h2>
+        </header>
+        <main>
+          <h3>Faça seu Login!</h3>
+          <label htmlFor="email">
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              data-testid="email-input"
+            />
+          </label>
+          <br />
+          <label htmlFor="senha">
+            <input
+              type="password"
+              id="senha"
+              placeholder="Senha"
+              data-testid="password-input"
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </main>
+      </div>
+    );
   }
 }
 
