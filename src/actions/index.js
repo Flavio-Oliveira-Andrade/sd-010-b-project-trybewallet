8da7df5407fba1e697f5440ff0a4ac82baacd4d1
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-09 10:52:40 -0300

    implementando actions requisição de API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,25 @@
 export const SUBMIT_EMAIL = 'SUBMIT_EMAIL';
 
+export const REQUEST_API = 'REQUEST_API';
+
+export const SAVE_CURRENCIES = 'SAVE_CURRENCIES';
+
 export const submitEmail = (email) => ({ type: SUBMIT_EMAIL, email });
+
+// export const requestCurrencies = () => ({ type: REQUEST_API });
+
+export const getCurrencies = (currencies) => ({ type: SAVE_CURRENCIES, currencies });
+
+export function fetchCurrencies() {
+  // dispatch(requestCurrencies());
+  return async (dispatch) => {
+    const request = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const response = await request.json();
+    const responseValues = Object.values(response);
+    const currenciesCode = responseValues.reduce((prev, curr) => {
+      const codes = prev;
+      return (curr.codein !== 'BRLT') ? codes.concat(curr.code) : codes;
+    }, []);
+    dispatch(getCurrencies(currenciesCode));
+  };
+}
