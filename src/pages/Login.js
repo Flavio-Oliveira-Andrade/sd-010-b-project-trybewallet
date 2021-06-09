057c8be57ef191bac5fdc06b5757cd61d7ae5ecc
aythan azevedo <aythan16@gmail.com>
2021-06-09 16:04:24 -0300

    pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Inputs from '../components/Inputs';
+
+import cambio from '../img/cambio.jpg';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form className="login">
+        <img src={ cambio } alt="wallet" width="185px" />
+        <br />
+        <Inputs place="Email" name="email" value="" test="email-input" />
+        <br />
+        <Inputs place="Senha" name="password" value="" test="password-input" />
+        <br />
+        <button type="button">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
