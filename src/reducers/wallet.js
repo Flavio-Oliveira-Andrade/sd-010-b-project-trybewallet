ff1a1479b47ed659e7c1fae8dc8eff4fa2faa746
Malbathan <malbathanlima@gmail.com>
2021-06-09 20:27:15 -0300

    Configurando CombineReducers

--- src/reducers/wallet.js
+++
@@ -1,7 +0,0 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const initialState =    {
-  wallet: {
-    currencies: [],
-    expenses: []
-  }
-};    
---
+++ src/redux/reducers/wallet.js
@@ -0,0 +1,16 @@
+// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  wallet: {
+    currencies: [],
+    expenses: []
+  }
+};
+
+export default function walletReducer(state = initialState, action) {
+  switch (action.type) {
+    // case value:
+    //   break;
+    default:
+      return state;
+  }
+}
