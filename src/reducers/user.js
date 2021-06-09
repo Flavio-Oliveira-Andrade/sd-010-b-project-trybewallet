5f50a73fe43121246bef187e7d87d7a3c022fc94
Malbathan <malbathanlima@gmail.com>
2021-06-09 19:23:39 -0300

    Definindo initialState dos Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,10 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+    user: {
+      email: '',
+    },
+    wallet: {
+      currencies: [],
+      expenses: []
+    }
+};
