1c62e558ea69f944c5225ccaed04fc83661d37db
RafaMaths <rmathscoach@gmail.com>
2021-06-09 20:53:42 -0300

    implements mapState for email in header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,10 +4,16 @@ class Wallet extends React.Component {
   render() {
     return (
       <div>
-        <header data-testid="total-field" />
+        <header>
+          <h1 data-testid="total-field">{ email }</h1>
+        </header>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state)({
+  email: state.user.email,
+});
 
 export default Wallet;
