f80b97a1d8ad78e075dbf26cd29edc4acc52f215
Felippe <felippecorrea2010@gmail.com>
2021-06-09 10:06:35 -0300

    Requisito 6: adicionando componentes controlados

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      expenses:'',
+      descrition: '',
+      coin:'',
+      payment:'money',
+      categories:'food',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target,
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     const { emailLogin } = this.props;
     return (
@@ -18,22 +39,26 @@ class Header extends React.Component {
               Valor
               <input
                 type="number"
-                value={}
+                value={ this.state.expenses }
                 name="expenses"
+                onChange={ this.handleChange }
               />
             </label>
             <label htmlFor="descrition">
               Descrição
               <input
                 type="string"
-                value={}
+                value={ this.state.descrition }
                 name="descrition"
+                onChange={ this.handleChange }
               />
             </label>
             <label htmlFor="coin">
               Moeda
               <select
                 name="coin"
+                value={ this.state.coin }
+                onChange={ this.handleChange }
               >
               </select>
             </label>
@@ -41,22 +66,26 @@ class Header extends React.Component {
               Método de Pagamento
               <select
                 name="payment"
+                value={ this.state.payment }
+                onChange={ this.handleChange }
               >
-                <option value={}>Dinheiro</option>
-                <option value={}>Cartão de Crédito</option>
-                <option value={}>Cartão de Débito</option>
+                <option value="money">Dinheiro</option>
+                <option value="creditCard">Cartão de Crédito</option>
+                <option value="debitCard">Cartão de Débito</option>
               </select>
             </label>
             <label htmlFor="categories">
               Tag
               <select
                 name="categories"
+                value={ this.state.categories }
+                onChange={ this.handleChange }
               >
-                <option value={}>Alimentação</option>
-                <option value={}>Lazer</option>
-                <option value={}>Trabalho</option>
-                <option value={}>Transporte</option>
-                <option value={}>Saúde</option>
+                <option value="food">Alimentação</option>
+                <option value="roby">Lazer</option>
+                <option value="job">Trabalho</option>
+                <option value="transport">Transporte</option>
+                <option value="hearth">Saúde</option>
               </select>
             </label>
           </form>
