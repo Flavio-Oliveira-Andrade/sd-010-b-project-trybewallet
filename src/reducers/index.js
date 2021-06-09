10486d03be08d7392b81341392ae1123a298df93
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-08 22:25:57 -0300

    finished 1,2,3

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import userReducer from './user';
 // import wallet from './wallet';
 const reducers = combineReducers({
-  userReducer,
+  user: userReducer,
 });
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,15 +1,13 @@
 import { STORE_EMAIL } from '../actions';
 
 const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
+  email: '',
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case STORE_EMAIL:
-    return { user: { ...state.user, email: action.payload.email } };
+    return { email: action.payload.email };
   default:
     return state;
   }
