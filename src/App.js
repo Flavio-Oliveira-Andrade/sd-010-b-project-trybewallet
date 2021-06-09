0f7d59dbb29a3d87505c77860ec59c33c96c9e16
isTorres <ismaeltorres612@gmail.com>
2021-06-09 11:15:51 -0300

    login page - email n password required - btn enable/disable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <Login />;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/wallet/:id" render={ () => <Wallet /> } />
+    </Switch>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.loginBtn = this.loginBtn.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  loginBtn() {
+    const { email, password } = this.state;
+    if (email === '' || password === '') {
+      return (
+        <button type="button" disabled>
+          Entrar
+        </button>
+      );
+    }
+    return (
+      <button type="button">
+        Entrar
+      </button>
+    );
+  }
+
+  handleChange({ value, name }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div className="App">
-        <input
-          data-testid="email-input"
-          type="text"
-          name="email"
-          maxLength="50"
-          placeholder="Email"
-          required
-        />
-        <input
-          data-testid="password-input"
-          type="password"
-          name="email"
-          maxLength="50"
-          placeholder="Senha"
-          required
-        />
+        <form className="Login">
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            maxLength="50"
+            placeholder="Email"
+            value={ email }
+            onChange={ (e) => this.handleChange(e.target) }
+            required
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            maxLength="50"
+            placeholder="Senha"
+            value={ password }
+            onChange={ (e) => this.handleChange(e.target) }
+            required
+          />
+          { this.loginBtn() }
+        </form>
       </div>
     );
   }
