0fa7eb1ceefd92fea8be48b14130fbbd7f3ce9ed
Carlos <cemargato@gmail.com>
2021-06-08 21:26:44 -0300

    requisito-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <>
       <div>Hello1, TrybeWallet!</div>
       <Switch>
+        <Route exact path="/carteira" component={ Wallet } />
         <Route exact path="/" component={ Login } />
       </Switch>
     </>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,67 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.emailChecker = this.emailChecker.bind(this);
+    this.passChecker = this.passChecker.bind(this);
+    this.test = this.test.bind(this);
+  }
+  // https://www.w3schools.com/jsref/jsref_regexp_test.asp
+  // ajuda do Zambs pra entender Regex
+
+  emailChecker({ target: { value } }) {
+    const emailFormat = RegExp(/[a-z0-9]+@[a-z0-9]+\.[a-z0-9]{2,3}(\.[a-z0-9]+)?$/);
+    if (emailFormat.test(value)) {
+      this.setState({
+        email: value,
+      });
+    }
+  }
+
+  passChecker({ target: { value } }) {
+    const six = 6;
+    if (value.length >= six) {
+      this.setState({
+        password: value,
+      });
+    }
+  }
+
+  test() {
+    let status = false;
+    const { email, password } = this.state;
+    if (email && password) {
+      status = false;
+    } else {
+      status = true;
+    }
+    return status;
+  }
+
   render() {
     return (
+
       <div>
         <h1>Login</h1>
 
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button type="button">Entrar</button>
+        <input
+          type="text"
+          name="email"
+          onChange={ this.emailChecker }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={ this.passChecker }
+          data-testid="password-input"
+        />
+        <button type="button" disabled={ this.test() }>Entrar</button>
       </div>
     );
   }
