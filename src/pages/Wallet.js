0095a47b0ed1d927c2c1495096b062ef50375024
bidaffonso <bidaffondo@gmail.com>
2021-06-10 18:51:42 -0400

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.genderForm = this.genderForm.bind(this);
+  }
+
+  genderForm() {
+    return (
+      <form>
+        <label htmlFor="wallet-value">
+          Valor:
+          <input type="number" name="value" id="wallet-value" />
+        </label>
+        <label htmlFor="wallet-description">
+          Descrição:
+          <input type="text" name="description" id="wallet-description" />
+        </label>
+        <label htmlFor="wallet-exchange">
+          Moeda:
+          <select name="exchange" id="wallet-exchange">
+            <option>CDR</option>
+          </select>
+        </label>
+        <label htmlFor="wallet-payment">
+          Método de pagamento:
+          <select name="payment" id="wallet-payment">
+            <option value="cash">Dinheiro</option>
+            <option value="credit">Cartão de crédito</option>
+            <option value="debit">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="wallet-tag">
+          Tag:
+          <select name="tag" id="wallet-tag">
+            <option value="food">Alimentação</option>
+            <option value="leisure">Lazer</option>
+            <option value="work">Trabalho</option>
+            <option value="transportation">Transporte</option>
+            <option value="health">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+
   render() {
     const { email } = this.props;
     return (
@@ -17,34 +61,10 @@ class Wallet extends React.Component {
             { 0 }
           </h3>
           <h3 data-testid="header-currency-field">
-            Câmbio em uso:
-            BRL
+            Câmbio em uso: BRL
           </h3>
         </header>
-        <form>
-          <label htmlFor="wallet-value">
-            Valor:
-            <input type="number" name="value" id="wallet-value" />
-          </label>
-          <label htmlFor="wallet-description">
-            Descrição:
-            <input type="text" name="description" id="wallet-description" />
-          </label>
-          <label htmlFor="wallet-exchange">
-            Moeda:
-            <select name="exchange" id="wallet-exchange">
-              <option>CDR</option>
-            </select>
-          </label>
-          <label htmlFor="wallet-payment">
-            Método de pagamento:
-            <select name="payment" id="wallet-payment">
-              <option value="cash">Dinheiro</option>
-              <option value="credit">Cartão de crédito</option>
-              <option value="debit">Cartão de débito</option>
-            </select>
-          </label>
-        </form>
+        {this.genderForm()}
       </main>
     );
   }
