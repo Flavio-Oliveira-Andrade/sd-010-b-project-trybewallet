5fe13748a8755824a7de80e47163fd697fb4299f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-10 15:46:34 -0300

    Renderiza o componente de inputValor e refatora as tags p

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Header from '../components/componentswallet/Header';
+import InputValue from '../components/componentswallet/InputValue';
 
 function Wallet() {
   const [totalDespesas] = useState(0);
@@ -8,12 +9,24 @@ function Wallet() {
   const [inputValue, setInputValue] = useState(0);
   const emailUser = useSelector((state) => state.user.email);
 
+  function handleInputValueChange(newValue) {
+    setInputValue(newValue);
+  }
+
   return (
-    <Header>
-      <p data-testid="email-field">{`Email: ${emailUser}`}</p>
-      <p data-testid="total-field">{`Total das despesas: ${totalDespesas}`}</p>
-      <p data-testid="header-currency-field">{`Moeda: ${cambio}`}</p>
-    </Header>
+    <>
+      <Header>
+        <span data-testid="email-field">{`Email: ${emailUser}`}</span>
+        <span data-testid="total-field">{`Total das despesas: ${totalDespesas}`}</span>
+        <span data-testid="header-currency-field">{`Moeda: ${cambio}`}</span>
+      </Header>
+
+      <InputValue
+        labelDescription="Valor"
+        inputValue={ inputValue }
+        onInputValueChange={ handleInputValueChange }
+      />
+    </>
   );
 }
 
