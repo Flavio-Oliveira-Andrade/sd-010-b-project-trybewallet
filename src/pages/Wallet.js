cbbd9ec1eb6db075cdf7454edf70eb531bb0f522
RafaMaths <rmathscoach@gmail.com>
2021-06-09 22:05:29 -0300

    implements select options in "payment" label

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -27,7 +27,11 @@ class Wallet extends React.Component {
           </label>
           <label htmlFor="payment">
             Método de pagamento
-            <input id="payment" />
+            <select id="payment">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
           </label>
 
         </form>
