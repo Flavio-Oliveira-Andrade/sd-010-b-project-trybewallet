7203c347536b5bc59baf553489f14093e2453e99
RafaMaths <rmathscoach@gmail.com>
2021-06-10 16:42:13 -0300

    create wallet reduce

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,29 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_API, GET_CURRENCY } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  // expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case REQUEST_API:
+    return {
+      ...state,
+      /* inserir loading */
+    };
+
+  case GET_CURRENCY:
+    return {
+      ...state,
+      currencies: action.data,
+      /* expenses: */
+
+    };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
