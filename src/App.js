daadd11e2b35734800b418d2db4d019f61f9dfb6
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-10 16:57:31 -0300

    Login forms created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <Route exact path="/" component={ Login } />
+      <Route />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './styles/Login.css';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login">
+        <h1>Login</h1>
+        <form>
+          <input type="email" placeholder="Email" data-testid="email-input" />
+          <input type="password" placeholder="Password" data-testid="password-input" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
