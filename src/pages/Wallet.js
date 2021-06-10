2cade6e576af81d40eb8e043fc4db627316ea7f4
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-10 19:48:39 -0300

    Fazendo a tabela e implementando state

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,28 +10,47 @@ class Wallet extends Component {
     this.expensesTotal = this.expensesTotal.bind(this);
 
     this.state = {
-      currency: 'BRL',
+      current: 'BRL',
     };
+  }
+
+  findProp(exchangeRates, currency) {
+    const findCurrency = Object.values(exchangeRates)
+      .find(({ code }) => currency === code);
+    return findCurrency;
+  }
+
+  calculeValue(exchangeRates, currency, value) {
+    if (value === '' || value === undefined) {
+      return 0;
+    }
+    const findCurrency = this.findProp(exchangeRates, currency);
+    const finalValue = parseFloat(findCurrency.ask) * parseFloat(value);
+    return finalValue;
+  }
+
+  reduceTotal(objCurrency) {
+    const result = objCurrency.reduce((acc, { value, currency, exchangeRates }) => {
+      if (value === '' || value === undefined) {
+        return 0;
+      }
+      const finalValue = this.findProp(exchangeRates, currency, value);
+      return acc + finalValue;
+    }, 0);
+    return result;
   }
 
   expensesTotal() {
     const { objCurrency } = this.props;
-
-    if (objCurrency.length >= 1) {
-      const result = objCurrency.reduce((acc, { value, currency, exchangeRates }) => {
-        const findCurrency = Object.values(exchangeRates)
-          .find(({ code }) => currency === code);
-        const finalValue = parseFloat(findCurrency.ask) * parseFloat(value);
-        return acc + finalValue;
-      }, 0);
-      return result;
+    if (objCurrency) {
+      this.reduceTotal(objCurrency);
     }
     return 0;
   }
 
   render() {
-    const { currency } = this.state;
-    const { login } = this.props;
+    const { current } = this.state;
+    const { login, objCurrency } = this.props;
     return (
       <div>
         <p data-testid="email-field">
@@ -41,9 +60,41 @@ class Wallet extends Component {
           { this.expensesTotal() }
         </span>
         <span data-testid="header-currency-field">
-          { currency }
+          { current }
         </span>
         <Form />
+        <table>
+          <thead>
+            <tr>
+              <td>Descrição</td>
+              <td>Tag</td>
+              <td>Método de pagamento</td>
+              <td>Valor</td>
+              <td>Moeda</td>
+              <td>Câmbio utilizado</td>
+              <td>Valor convertido</td>
+              <td>Moeda de conversão</td>
+              <td>Editar</td>
+              <td>Excluir</td>
+            </tr>
+          </thead>
+          <tbody>
+            { objCurrency && objCurrency.map((
+              { id, description, value, currency, method, tag, exchangeRates },
+            ) => (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ !value ? 0 : value }</td>
+                <td>{ (this.findProp(exchangeRates, currency).name).split('/')[0] }</td>
+                <td>{ this.findProp(exchangeRates, currency).ask }</td>
+                <td>{ this.calculeValue(exchangeRates, currency, value) }</td>
+                <td>Real</td>
+              </tr>
+            )) }
+          </tbody>
+        </table>
       </div>
     );
   }
