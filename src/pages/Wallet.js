7ca6eca7bc14d9ae57db31310e0f9cff6ffce783
Daniel Ceci <danielceci96@gmail.com>
2021-06-10 00:45:25 -0300

    testando uma mudança pequena  no requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -17,6 +17,6 @@ Wallet.propTypes = {
 };
 // o mapStateToProps é pra que a gente consiga pegar os valores que estão nos estados globais
 const mapStateToProps = (state) => ({
-  email: state.usuario.email,
-}); // essa é a estrutura do mapStateToProps. O email vem do estado global  e aqui eu trouxe por Props. O usuario foi a função lá do dispatch no Login.js
+  email: state.user.email,
+}); // essa é a estrutura do mapStateToProps. O email vem do estado global  e aqui eu trouxe por Props.
 export default connect(mapStateToProps, null)(Wallet);
