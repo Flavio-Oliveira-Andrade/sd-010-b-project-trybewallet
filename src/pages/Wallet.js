5d86fcca0d96ddade0182a3cd405ea1fc2f243e6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-10 17:16:44 -0300

    Refatorando o componente wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,26 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import FormsExpenses from '../components/componentswallet/FormsExpenses';
 import Header from '../components/componentswallet/Header';
-import InputDescriptionExpenses
-  from '../components/componentswallet/InputDescriptionExpenses';
-import InputSelectMethod from '../components/componentswallet/InputSelectMethod';
-import InputSelectMoeda from '../components/componentswallet/InputSelectMoeda';
-import InputValue from '../components/componentswallet/InputValue';
 
 function Wallet() {
   const [totalDespesas] = useState(0);
   const [cambio] = useState('BRL');
-  const [inputValue, setInputValue] = useState(0);
-  const [inputDescription, setInputDescripion] = useState('');
   const emailUser = useSelector((state) => state.user.email);
-
-  function handleInputValueChange(newValue) {
-    setInputValue(newValue);
-  }
-
-  function handleInputDescriptionChange(newDescription) {
-    setInputDescripion(newDescription);
-  }
 
   return (
     <>
@@ -30,27 +16,7 @@ function Wallet() {
         <span data-testid="header-currency-field">{`Moeda: ${cambio}`}</span>
       </Header>
 
-      <form>
-        <InputValue
-          labelDescription="Valor"
-          inputValue={ inputValue }
-          onInputValueChange={ handleInputValueChange }
-        />
-
-        <InputDescriptionExpenses
-          labelDescription="Descrição"
-          inputValue={ inputDescription }
-          onInputDescriptionChange={ handleInputDescriptionChange }
-        />
-
-        <InputSelectMoeda
-          labelDescription="Moeda"
-        />
-
-        <InputSelectMethod
-          labelDescription="Método de pagamento"
-        />
-      </form>
+      <FormsExpenses />
     </>
   );
 }
