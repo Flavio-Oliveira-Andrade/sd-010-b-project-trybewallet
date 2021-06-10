46b18dc74d81ea06b45c5ec409d844800c380bef
Anderson <trkz.anderson@gmail.com>
2021-06-10 19:14:23 -0300

    Retirando loadedPage

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -118,49 +118,45 @@ class Wallet extends React.Component {
 
   render() {
     const { expense } = this.state;
-    const { email, dispatchAddExpense, loadedPage } = this.props;
+    const { email, dispatchAddExpense } = this.props;
 
-    if (loadedPage) {
-      return (
-        <>
-          <header>
-            <span data-testid="email-field">{`Email: ${email} `}</span>
-            <span data-testid="total-field">
-              { `Total: ${this.loadTotalWalletExpenses()} `}
-            </span>
-            <span data-testid="header-currency-field">BRL</span>
-          </header>
+    return (
+      <>
+        <header>
+          <span data-testid="email-field">{`Email: ${email} `}</span>
+          <span data-testid="total-field">
+            { `Total: ${this.loadTotalWalletExpenses()} `}
+          </span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
 
-          <main>
-            { this.generateExpenseForm() }
-          </main>
-          <main>
-            <table>
-              <tr>
-                <th>Descrição</th>
-                <th>Tag</th>
-                <th>Método de pagamento</th>
-                <th>Valor</th>
-                <th>Moeda</th>
-                <th>Câmbio utilizado</th>
-                <th>Valor convertido</th>
-                <th>Moeda de conversão</th>
-                <th>Editar/Excluir</th>
-              </tr>
-              { this.populateTableExpenses() }
-            </table>
-          </main>
+        <main>
+          { this.generateExpenseForm() }
+        </main>
+        <main>
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+            { this.populateTableExpenses() }
+          </table>
+        </main>
 
-          <footer>
-            <button type="button" onClick={ () => dispatchAddExpense(expense) }>
-              Adicionar Despesa
-            </button>
-          </footer>
-        </>
-      );
-    }
-
-    return <h1>Carregando...</h1>;
+        <footer>
+          <button type="button" onClick={ () => dispatchAddExpense(expense) }>
+            Adicionar Despesa
+          </button>
+        </footer>
+      </>
+    );
   }
 }
 
