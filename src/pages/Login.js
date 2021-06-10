e4c727bf2da35daf966eb1abae17f0f55c331b0c
Flavio Andrade <foa12381@gmail.com>
2021-06-10 17:41:14 -0300

    resolv

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -23,49 +23,45 @@ class Login extends React.Component {
     const { email, password } = this.state;
     const emailValido = email.match(/^[^ ]+@[^ ]+.[a-z]{2,3}$/);
     return (
-      <div className="container-login">
-        <div className="wrap-login">
-          <div>
-            <section className="slogan-container">
-              <h1>Sua Cotação </h1>
-              <h3>Em qualquer Moeda</h3>
-            </section>
-          </div>
-          <form>
-            <span className="login-form-title">
-              Faça seu Login
-            </span>
-            <div className="wrap-input">
-              <input
-                data-testid="email-input"
-                type="email"
-                id="email"
-                placeholder="Email"
-                onChange={ this.handleChange }
-              />
-            </div>
-            <div className="wrap-input">
-              <input
-                data-testid="email-input"
-                type="password"
-                id="password"
-                placeholder="Password"
-                maxLength="12"
-                onChange={ this.handleChange }
-              />
-            </div>
-            <Link to="carteira">
-              <button
-                type="button"
-                className="login-form-btn"
-                onClick={ () => console.log('oi') }
-                disabled={ !emailValido || password.length <= '5' }
-              >
-                Entrar
-              </button>
-            </Link>
-          </form>
-        </div>
+      <div>
+        Login
+        <br />
+        <label htmlFor="email">
+          <br />
+          E-mail:
+          {' '}
+          <input
+            id="email"
+            type="email"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <br />
+          Senha:
+          {'      '}
+          <input
+            id="password"
+            type="password"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        {'     '}
+        <Link to="/carteira">
+          <button
+            type="button"
+            className="login-form-btn"
+            onClick={ () => console.log('oi') }
+            disabled={ !emailValido || password.length <= '5' }
+          >
+            Entrar
+          </button>
+        </Link>
+
       </div>
     );
   }
