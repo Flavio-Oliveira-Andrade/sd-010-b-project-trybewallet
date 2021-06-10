0b334f6905c6229b755238eeb67d83c397c808e4
Renan0211 <renan.antunes.rocha@gmail.com>
2021-06-10 18:35:17 -0300

    Implementa a logica de salvar o input de nova despesa no estado global

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import fetchCurrency from '../services/fetchCurrency';
+import { actionFetchCurrencies, newExpense } from '../actions';
 
 class Wallet extends React.Component {
   constructor(props) {
@@ -9,7 +10,16 @@ class Wallet extends React.Component {
     this.calculateTotal = this.calculateTotal.bind(this);
     this.setCurrencyOnState = this.setCurrencyOnState.bind(this);
     this.returnCurrencies = this.returnCurrencies.bind(this);
-    this.state = { currencies: {} };
+    this.handleChange = this.handleChange.bind(this);
+    this.addCost = this.addCost.bind(this);
+    this.state = {
+      currencies: {},
+      expenseValue: '',
+      expenseDescription: '',
+      currency: 'USD',
+      payMeth: 'Alimentação',
+      expenseTag: 'Dinheiro',
+    };
   }
 
   async componentDidMount() {
@@ -19,6 +29,11 @@ class Wallet extends React.Component {
 
   setCurrencyOnState(currencies) {
     this.setState({ currencies });
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   returnCurrencies(currencies) {
@@ -40,14 +55,79 @@ class Wallet extends React.Component {
     return total;
   }
 
-  rerturnOi() {
-    return <option>Oi</option>;
+  async addCost() {
+    const { expenseValue,
+      expenseDescription, expenseTag, payMeth, currency } = this.state;
+    const { dispatchFetchCurrencies, dispatchNewExpense, userExpenses } = this.props;
+    dispatchFetchCurrencies();
+    const exchangeRates = await fetchCurrency();
+    const lastExpense = userExpenses[userExpenses.length - 1];
+    const expenseToAdd = {
+      id: lastExpense ? lastExpense.id + 1 : 0,
+      expenseValue,
+      expenseDescription,
+      expenseTag,
+      payMeth,
+      currency,
+      exchangeRates,
+    };
+    dispatchNewExpense(expenseToAdd);
+  }
+
+  returnForm() {
+    const { currencies } = this.state;
+    return (
+      <form>
+        <label htmlFor="expense-value">
+          Valor:
+          <input
+            type="number"
+            id="expense-value"
+            name="expenseValue"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="expense-description">
+          Descrição:
+          <input
+            type="text"
+            id="expense-description"
+            name="expenseDescription"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="moeda">
+          Moeda
+          <select id="moeda" name="currency" onChange={ this.handleChange }>
+            {this.returnCurrencies(currencies)}
+          </select>
+        </label>
+        <label htmlFor="método de pagamento">
+          Método de Pagamento
+          <select id="método de pagamento" name="payMeth" onChange={ this.handleChange }>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="expense-tag">
+          Tag
+          <select name="expenseTag" id="expense-tag" onChange={ this.handleChange }>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <button type="button" onClick={ this.addCost }>Adicionar Despesa</button>
+      </form>
+    );
   }
 
   render() {
     const { userEmail } = this.props;
     const total = this.calculateTotal();
-    const { currencies } = this.state;
     return (
       <div>
         <header>
@@ -55,40 +135,7 @@ class Wallet extends React.Component {
           <section data-testid="total-field">{`Total: ${total}`}</section>
           <section data-testid="header-currency-field">Moeda atual: BRL</section>
         </header>
-        <form>
-          <label htmlFor="expense-value">
-            Valor:
-            <input type="number" id="expense-value" name="expenseValue" />
-          </label>
-          <label htmlFor="expense-description">
-            Descrição:
-            <input type="text" id="expense-description" name="expenseDescription" />
-          </label>
-          <label htmlFor="expense-currency">
-            Moeda
-            <select id="expense-currency">
-              {this.returnCurrencies(currencies)}
-            </select>
-          </label>
-          <label htmlFor="expense-payment-method">
-            Método de Pagamento
-            <select id="expense-payment-method">
-              <option value="cash">Dinheiro</option>
-              <option value="credit-card">Cartão de crédito</option>
-              <option value="debit-card">Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="expense-tag">
-            Tag
-            <select name="expenseTag" id="expense-tag">
-              <option value="food">Alimentação</option>
-              <option value="leisure">Lazer</option>
-              <option value="work">Trabalho</option>
-              <option value="transportation">Transporte</option>
-              <option value="health">Saúde</option>
-            </select>
-          </label>
-        </form>
+        {this.returnForm()}
       </div>
     );
   }
@@ -97,11 +144,19 @@ class Wallet extends React.Component {
 Wallet.propTypes = {
   userEmail: PropTypes.string.isRequired,
   userExpenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  dispatchFetchCurrencies: PropTypes.func.isRequired,
+  dispatchNewExpense: PropTypes.func.isRequired,
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchNewExpense: (state) => dispatch(newExpense(state)),
+  dispatchFetchCurrencies: () => dispatch(actionFetchCurrencies()),
+});
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
   userExpenses: state.wallet.expenses,
+  currencies: state.wallet.currencies,
 });
 
-export default connect(mapStateToProps, null)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
