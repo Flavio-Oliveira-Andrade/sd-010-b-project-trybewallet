82f9d242bb687ce2e0026fd4d682ac974747b4d0
Mariana Savoldi <marianasavodi123@gmail.com>
2021-06-10 17:36:29 -0300

    Requisito 2 completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      isValidEmail: false,
+      isValidSenha: false,
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangeSenha = this.handleChangeSenha.bind(this);
+  }
+
+  handleChangeEmail({ target: { value } }) {
+    this.setState({
+      email: value,
+    });
+    // if (value.match(/[a-z]+@[a-z]+.com/g)) {
+    //   this.setState({ isValidEmail:  });
+    // }
+    this.setState({ isValidEmail: value.match(/[a-z]+@[a-z]+.com/g) });
+  }
+
+  handleChangeSenha({ target: { value } }) {
+    const minLength = 6;
+    // if (value.length >= minLength) {
+    //   this.setState({ isValidSenha: true });
+    // } else {
+    //   this.setState({ isValidSenha: false });
+    // }
+    this.setState({ isValidSenha: value.length >= minLength });
+  }
+
   render() {
+    const { email, isValidEmail, isValidSenha } = this.state;
+    const enable = isValidEmail && isValidSenha;
+    console.log(email);
     return (
       <div className="App">
         <header>
@@ -15,6 +49,7 @@ class Login extends React.Component {
               id="email"
               placeholder="Email"
               data-testid="email-input"
+              onChange={ this.handleChangeEmail }
             />
           </label>
           <br />
@@ -24,9 +59,10 @@ class Login extends React.Component {
               id="senha"
               placeholder="Senha"
               data-testid="password-input"
+              onChange={ this.handleChangeSenha }
             />
           </label>
-          <button type="button">Entrar</button>
+          <button type="button" disabled={ !enable }>Entrar</button>
         </main>
       </div>
     );
