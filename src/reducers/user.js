356eb0dce994c4821dce069540f5eddd8764b237
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-10 11:24:59 -0300

    Cria o reducer de manipulação do email

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,14 @@
-const INTIAL_LOGIN = { email: '' };
+const INTIAL_LOGIN = {
+  email: null,
+};
 
 function userReducer(state = INTIAL_LOGIN, action) {
   switch (action.type) {
+  case 'EMAIL_LOGIN':
+    return {
+      ...state,
+      email: action.email,
+    };
   default:
     return state;
   }
