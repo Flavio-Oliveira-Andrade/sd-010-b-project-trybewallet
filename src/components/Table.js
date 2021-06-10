754654c796705c60ec6105c010faff21a91ec2c1
Shayalla <77649441+Shayalla@users.noreply.github.com>
2021-06-09 23:05:32 -0300

    Agr vai lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,7 +10,7 @@ class Table extends React.Component {
   }
   // componente pego do repositório do Thiago Marchini
   // https://github.com/tryber/sd-010-b-project-trybewallet/pull/1/commits/10b4ba74b098312c0cb053a29ecac8ae785fd4f1
-  
+
   render() {
     const { expenses } = this.props;
     console.log(expenses);
