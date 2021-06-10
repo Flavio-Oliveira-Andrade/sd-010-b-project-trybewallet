7bb7cca28b02c9a21c5647596fe1bd88fcf30b09
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-10 18:04:27 -0300

    created Login local state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+
+export const changeEmail = (state) => (
+  { type: 'INPUT_EMAIL', payload: { email: state } }
+);
+export const changePassword = (state) => (
+  { type: 'INPUT_PASSWORD', payload: { password: state } }
+);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './styles/Login.css';
+import { changeEmail, changePassword } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   isButtonDisabled(userEmail, userPassword) {
     // Regex from https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -16,12 +33,27 @@ class Login extends React.Component {
 
   render() {
     const { userEmail, userPassword } = this.props;
+    const { email, password } = this.state;
     return (
       <div className="Login">
         <h1>Login</h1>
         <form>
-          <input type="email" placeholder="Email" data-testid="email-input" />
-          <input type="password" placeholder="Password" data-testid="password-input" />
+          <input
+            name="email"
+            type="email"
+            value={ email }
+            placeholder="Email"
+            data-testid="email-input"
+            onChange={ this.handleOnChange }
+          />
+          <input
+            name="password"
+            type="password"
+            value={ password }
+            placeholder="Password"
+            data-testid="password-input"
+            onChange={ this.handleOnChange }
+          />
           <button
             type="submit"
             disabled={ this.isButtonDisabled(userEmail, userPassword) }
@@ -39,9 +71,14 @@ const mapStateToProps = ({ userReducer: { user: { email, password } } }) => ({
   userPassword: password,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  changeEmail: (state) => dispatch(changeEmail(state)),
+  changePassword: (state) => dispatch(changePassword(state)),
+});
+
 Login.propTypes = {
   userEmail: PropTypes.string.isRequired,
   userPassword: PropTypes.string.isRequired,
 };
 
-export default connect(mapStateToProps, null)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
