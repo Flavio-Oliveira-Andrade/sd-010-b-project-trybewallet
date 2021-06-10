1e4c9d2a3bec853f110381c882a2d472edaba041
Pedro Amaral <amaralph1995@gmail.com>
2021-06-10 18:28:37 -0300

    Cria header e conteudos

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+
+import '../style/wallet.css';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      total: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    const { total } = this.state;
+    return (
+      <div>
+        <header className="header">
+          <h3 data-testid="email-field">{ email }</h3>
+          <div className="currency">
+            <h3 data-testid="total-field">{ total }</h3>
+            <h3 data-testid="header-currency-field">BRL</h3>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Wallet);
