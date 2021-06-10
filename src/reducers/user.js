d7a7c980cb3766df72356927202ad77b90747a8b
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-10 17:16:33 -0300

    user and wallet reducers and store created

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER = {
+  user: {
+    email: '',
+    password: '',
+  },
+};
+
+function user(state = INITIAL_USER, action) {
+  switch (action.type) {
+  case 'something':
+    return state;
+  default:
+    return state;
+  }
+}
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_WALLET = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function wallet(state = INITIAL_WALLET, action) {
+  switch (action.type) {
+  case 'something':
+    return state;
+  default:
+    return state;
+  }
+}
+
+export default wallet;
