06a9f76f040d6abc8560e8c44a1bc99254ba6954
João Victor Pistorio <joao.pistorio@gmail.com>
2021-06-10 15:37:16 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './store';
+
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter store={ store }>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1>Trybe Login</h1>
+
+        <label htmlFor="email-login">
+          Email
+          <input
+            id="email-login"
+            type="text"
+            name="email"
+            placeholder="email@email.com"
+            data-testid="email-input"
+          />
+        </label>
+
+        <label htmlFor="password-login">
+          Senha
+          <input
+            id="password-login"
+            type="password"
+            name="password"
+            placeholder="informe sua senha"
+            data-testid="password-input"
+          />
+        </label>
+
+        <button type="button">Entrar</button>
+
+      </div>
+    );
   }
 }
 
