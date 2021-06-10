062d546ca0d17a9363a8dd601276aace3d68784d
Lucas Ribeiro <lucasrb04@gmail.com>
2021-06-10 11:01:44 -0300

    adicionando correçao lint

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <Switch>
       <Route
+        path="/carteira"
+        render={ (reactRouterProps) => (<Wallet { ...reactRouterProps } />) }
+      />
+      <Route
         path="/"
-        render={ () => (<Login />) }
+        render={ (reactRouterProps) => (<Login { ...reactRouterProps } />) }
       />
     </Switch>
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productInfo: {},
+      isLoading: true,
+      quantity: props.itemObj.quantidade,
+    };
+  }
+  
+  decreaseItem = () => {
+    const { quantity } = this.state;
+    if (quantity > 1) {
+      this.setState({ quantity: quantity - 1 });
+    }
+  }
+
   render() {
     return (
       <div>
@@ -15,7 +31,12 @@ class Login extends React.Component {
             <input data-testid="password-input" type="password" name="senha" />
           </label>
           <Link to="/carteira">
-            <button type="submit">Entrar</button>
+            <button
+              type="submit"
+              // disabled={ }
+            >
+              Entrar
+            </button>
           </Link>
         </form>
       </div>
