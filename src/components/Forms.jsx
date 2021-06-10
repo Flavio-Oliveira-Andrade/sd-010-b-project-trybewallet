1f41615fc4f2b610b615f3daa37a1716a4105985
RafaMaths <rmathscoach@gmail.com>
2021-06-10 15:15:58 -0300

    create component Forms

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -5,11 +5,41 @@ class Forms extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1 data-testid="email-field">{ email }</h1>
-          <h3 data-testid="total-field">0</h3>
-          <h4 data-testid="header-currency-field">BRL</h4>
-        </header>
+        <form>
+          <label htmlFor="amount">
+            Valor
+            <input type="number" name="amount" id="amount" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" name="description" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <input name="currency" id="currency" />
+            <select>
+              <option value="BRL">BRL</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select name="payment" id="payment">
+              <option value="money">Dinheiro</option>
+              <option value="credit card">Cartão de crédito</option>
+              <option value="debit card">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select name="tag" id="tag">
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,47 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
-    return (      
-        <form>
-          <label htmlFor="amount">
-            Valor
-            <input type="number" name="amount" id="amount" />
-          </label>
-          <label htmlFor="description">
-            Descrição
-            <input type="text" name="description" id="description" />
-          </label>
-          <label htmlFor="currency">
-            Moeda
-            <input name="currency" id="currency" />
-            <select>
-              <option value="BRL">BRL</option>
-            </select>
-          </label>
-          <label htmlFor="payment">
-            Método de pagamento
-            <select name="payment" id="payment">
-              <option value="money">Dinheiro</option>
-              <option value="credit card">Cartão de crédito</option>
-              <option value="debit card">Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="tag">
-            Tag
-            <select name="tag" id="tag">
-              <option value="food">Alimentação</option>
-              <option value="leisure">Lazer</option>
-              <option value="work">Trabalho</option>
-              <option value="transport">Transporte</option>
-              <option value="health">Saúde</option>
-            </select>
-          </label>
-        </form>
-      </div>
+    return (
+      <main>
+        <Header />
+
+      </main>
     );
   }
 }
