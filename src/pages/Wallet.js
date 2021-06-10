e7200ae74b9d47b2dfaefb47fc055be79951eafb
renanpbraga <renanpbraga@gmail.com>
2021-06-10 19:37:16 -0300

    adiciona requisitos 4 e 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>WALLET</div>;
+    const { userEmail } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{ userEmail }</p>
+        <label htmlFor="input-despesa-total">
+          Despesas totais:
+          <input name="input-despesa-total" type="number" value="0" data-testid="total-field" />
+        </label>
+        <label htmlFor="actual-currency">
+          Moeda atual:
+          <select name="actual-currency" data-testid="header-currency-field">
+            <option value="BRL">BRL</option>
+          </select>
+        </label>
+
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
