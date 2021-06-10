352491d175c17610466249e8754db4dad43bbc60
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-06-10 16:41:19 -0300

    Arrumando erro de lint

--- src/components/FormsDespesa.jsx
+++ src/components/FormsDespesa.jsx
@@ -11,7 +11,7 @@ class FormsDespesa extends Component {
       description: '',
       tag: 'Alimentação',
       currency: 'USD',
-      metodoPagamento: 'Dinheiro',
+      payment: 'Dinheiro',
     };
     this.handleChange = this.handleChange.bind(this);
     this.addDispesa = this.handleChange.bind(this);
@@ -28,57 +28,100 @@ class FormsDespesa extends Component {
   }
 
   addDispesa() {
+  }
 
+  renderValueLabel() {
+    const { value } = this.state;
+    return (
+      <label htmlFor="value">
+        Valor
+        <input
+          type="text"
+          name="value"
+          id="value"
+          value={ value }
+          onChange={ (e) => this.handleChange(e) }
+        />
+      </label>);
+  }
+
+  renderDescriptionLabel() {
+    const { description } = this.state;
+    return (
+      <label htmlFor="description">
+        Descrição
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={ description }
+          onChange={ (e) => this.handleChange(e) }
+        />
+      </label>);
+  }
+
+  renderCurrencyLabel() {
+    const { currencies } = this.props;
+    const { currency } = this.state;
+    const curren = currencies.filter((c) => c !== 'USDT');
+    return (
+
+      <label htmlFor="currency">
+        Moeda
+        <select
+          name="currency"
+          id={ currency }
+          value={ currency }
+          onChange={ (e) => this.handleChange(e) }
+        >
+          {curren.map((curr, index) => (
+            <option key={ index } value={ curr }>{curr}</option>
+          ))}
+        </select>
+      </label>);
+  }
+
+  renderPaymentLabel() {
+    const { payment } = this.state;
+    return (
+      <label htmlFor="payment">
+        Método de pagamento
+        <select name="payment" id={ payment } value={ payment }>
+          <option value="dinheiro">Dinheiro</option>
+          <option value="debito">Cartão de Debito</option>
+          <option value="credito">Cartão de Credito</option>
+        </select>
+      </label>);
+  }
+
+  renderTagLabel() {
+    const { tag } = this.state;
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select name="tag" value={ tag }>
+          <option value="alimentacao">Lazer</option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+      </label>);
   }
 
   render() {
-    const { currencies, loading } = this.props;
-    console.log(currencies);
-    const { tag, currency, metodoPagamento, description, value } = this.state;
-    const curren = currencies.filter((c) => c !== 'USDT');
     return (
-      <div>
-        <form>
-          <label htmlFor="value">
-            Valor
-            <input type="text" name="value" id="value" value={ value } onChange={ (e) => this.handleChange(e) } />
-          </label>
-
-          <label htmlFor="description">
-            Descrição
-            <input type="text" name="description" id="description" value={ description } onChange={ (e) => this.handleChange(e) } />
-          </label>
-
-          <label htmlFor="currency">
-            Moeda
-            <select name="currency" value={ currency } onChange={ (e) => this.handleChange(e) }>
-              {curren.map((curr, index) => (
-                <option key={ index } value={ curr }>{curr}</option>
-              ))}
-            </select>
-          </label>
-
-          <label htmlFor="metodoPagamento">
-            Método de pagamento
-            <select name="metodoPagamento" value={ metodoPagamento }>
-              <option value="dinheiro">Dinheiro</option>
-              <option value="debito">Cartão de Debito</option>
-              <option value="credito">Cartão de Credito</option>
-            </select>
-          </label>
-
-          <label htmlFor="tag">
-            Tag
-            <select name="tag" value={ tag }>
-              <option value="alimentacao">Lazer</option>
-              <option value="trabalho">Trabalho</option>
-              <option value="transporte">Transporte</option>
-              <option value="saude">Saúde</option>
-            </select>
-          </label>
-          <button onClick={ () => this.addDispesa } type="submit">Adicionar Despesa</button>
-        </form>
-      </div>
+      <form>
+        {this.renderValueLabel()}
+        ;
+        {this.renderDescriptionLabel()}
+        ;
+        {this.renderCurrencyLabel()}
+        ;
+        {this.renderPaymentLabel()}
+        ;
+        {this.renderTagLabel()}
+        <button onClick={ () => this.addDispesa } type="submit">Adicionar Despesa</button>
+      </form>
     );
   }
 }
