90aaa68625ad07ae5cd306f3ed8b57b12eab0fd1
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-06-10 17:23:33 -0300

    Correcao requisito 8

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -72,22 +72,24 @@ class AddExpense extends React.Component {
           <label htmlFor="metodo">
             Método de pagamento
             <select id="metodo">
-              <option value="dinheiro">Dinheiro</option>
-              <option value="credito">Cartão de crédito</option>
-              <option value="debito">Cartão de débito</option>
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
             </select>
           </label>
           <label htmlFor="tag">
             Tag
             <select id="tag">
-              <option value="alimentacao">Alimentação</option>
-              <option value="lazer">Lazer</option>
-              <option value="trabalho">Trabalho</option>
-              <option value="transporte">Transporte</option>
-              <option value="saude">Saúde</option>
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
             </select>
           </label>
-          <input type="button" value="Adicionar" onClick={ () => this.handleClick() } />
+          <button type="button" onClick={ () => this.handleClick() }>
+            Adicionar despesa
+          </button>
         </form>
       </div>
     );
