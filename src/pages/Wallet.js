340c1c38f8614d9dfd764f1cf703c19d375e8a17
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-10 13:53:43 -0300

    Cria o estado do componente com as despesas e cabio da moeda

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '../components/componentswallet/Header';
 
 function Wallet() {
+  const [totalDespesas, setTotalDespesas] = useState(0);
+  const [cambio, setCambio] = useState('BRL');
+  const emailUser = useSelector((state) => state.user.email);
   return (
     <Header />
   );
