c177a0205d21aa5a46d0bbc8c851bb1672ce3d4f
Felippe <felippecorrea2010@gmail.com>
2021-06-11 10:19:38 -0300

    Criando componente Expenses para renderizar as despesas na carteira

---
+++ src/components/Expenses.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Expenses extends React.Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <section>
+        {expenses.map((expense, key) => (
+          <div key={ key }>
+            <span>{ expense.expenses }</span>
+          </div>
+        ))}
+      </section>
+    );
+  }
+}
+
+Expenses.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Expenses);
