bb4daba95d3962bd5f15229c82ebb0a593c17eab
Pedro Toselli <pedrotrybe@gmail.com>
2021-06-11 00:06:51 -0300

    Requisito 6 - Completo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,7 +10,80 @@ class Wallet extends React.Component {
     this.state = {
       dispenses: 0,
       currency: 'BRL',
+      description: '',
     };
+  }
+
+  createValue() {
+    const { dispenses } = this.state;
+
+    return (
+      <label htmlFor="value">
+        Valor
+        <input
+          name="value"
+          value={ dispenses }
+          id="value"
+        />
+      </label>
+    );
+  }
+
+  createDescription() {
+    const { description } = this.state;
+
+    return (
+      <label htmlFor="description">
+        Descrição
+        <input
+          name="description"
+          value={ description }
+          id="description"
+        />
+      </label>
+    );
+  }
+
+  createCurrency() {
+    return (
+      <label htmlFor="currency">
+        Moeda
+        <select
+          id="currency"
+          role="combobox"
+          name="currency"
+          >
+        </select>
+      </label>
+    );
+  }
+
+  createPaymentMethod() {
+    return (
+      <label htmlFor="payment">
+        Método de pagamento
+        <select id="payment" role="combobox" name="payment">
+          <option value="money">Dinheiro</option>
+          <option value="credit-card">Cartão de crédito</option>
+          <option value="debit-card">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  createCategory() {
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select id="tag" role="combobox" name="tag">
+          <option value="alimentacao">Alimentação</option>
+          <option value="lazer">Lazer</option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+      </label>
+    );
   }
 
   render() {
@@ -24,36 +97,11 @@ class Wallet extends React.Component {
           <h2 data-testid="header-currency-field">{ currency }</h2>
         </header>
         <form>
-          <label htmlFor="value">
-            Valor
-            <input type="text" name="value" />
-          </label>
-          <label htmlFor="description">
-            Descrição
-            <input type="text" name="description" />
-          </label>
-          <label htmlFor="currency">
-            Moeda
-            <select role="combobox" name="currency"> </select>
-          </label>
-          <label htmlFor="payment">
-            Método de pagamento
-            <select role="combobox" name="payment">
-              <option value="money">Dinheiro</option>
-              <option value="credit-card">Cartão de crédito</option>
-              <option value="debit-card">Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="currency">
-            Tag
-            <select role="combobox" name="currency">
-              <option value="alimentacao">Alimentação</option>
-              <option value="lazer">Lazer</option>
-              <option value="trabalho">Trabalho</option>
-              <option value="transporte">Transporte</option>
-              <option value="saude">Saúde</option>
-            </select>
-          </label>
+          { this.createValue() }
+          { this.createDescription() }
+          { this.createCurrency() }
+          { this.createPaymentMethod() }
+          { this.createCategory() }
         </form>
       </>
     );
