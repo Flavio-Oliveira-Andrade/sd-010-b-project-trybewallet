52ba2818db15b666e38e71e1d64703134a6dafff
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-11 02:05:27 -0300

    inserido o .type na action do swich

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
-  switch (action) {
+  switch (action.type) {
   case SELECT_CURRENCIES:
     return {
       ...state,
