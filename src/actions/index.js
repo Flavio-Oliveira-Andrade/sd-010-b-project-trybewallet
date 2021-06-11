eeab580092037b50ab4289a1f2ba1fea3d3e077c
isTorres <ismaeltorres612@gmail.com>
2021-06-10 21:46:48 -0300

    clean file coments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
-// Coloque aqui suas actions
 export const ADD_USER = 'ADD_USER';
-
 export const GET_TOTAL = 'GET_TOTAL';
+export const GET_API = 'GET_API';
 
+// action for Login
 export const addUsername = (email) => ({
   type: ADD_USER,
   payload: email,
 });
+
+export const getAPI = () => ({ type: GET_API });
