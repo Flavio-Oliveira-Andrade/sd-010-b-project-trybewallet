b95a5f9b4bbb7172b5fd91646f63f6d0dba6a749
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-06-11 14:10:34 -0300

    configuraçoes inciais do redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />
+  document.getElementById('root'),
+);
