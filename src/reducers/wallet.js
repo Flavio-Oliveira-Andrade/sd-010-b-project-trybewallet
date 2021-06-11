2aefb0c80fdb4777b9197f926ae2d677018633cf
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-11 19:36:15 -0300

    Cria reducer para a requisisão que salva na store

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,11 +1,23 @@
 const INITIAL_STATE = {
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
+  currencies: [],
+  expenses: [{
+    id: 0,
+    value: 0,
+    description: 'Fioravante',
+    currency: '',
+    method: '',
+    tag: '',
+    exchangeRates: {},
+  }],
 };
 function walletReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case 'SAVE_EXPENSES':
+    return {
+      ...state,
+      expenses: state.expenses
+        .map((expense) => ({ ...expense, exchangeRates: { ...action.payload } })),
+    };
   default:
     return state;
   }
