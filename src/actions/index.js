baaddce275beb148d423551c489290a8766c2273
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-11 19:33:05 -0300

    Cria Action que faz a requisição

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-const saveEmailLogin = (email) => ({
+import requestApi from '../services/api';
+
+export const saveEmailLogin = (email) => ({
   type: 'EMAIL_LOGIN',
   email,
 });
 
-export default saveEmailLogin;
+export const saveExpenses = () => async (dispatch) => {
+  const response = await requestApi();
+  dispatch({
+    type: 'SAVE_EXPENSES',
+    payload: response,
+  });
+};
