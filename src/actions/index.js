38c905e02bc56c1d7d605eb76f8001642acc95f1
root <diegoserafim1@gmail.com>
2021-06-10 21:55:10 -0300

    criado a conexao com a API para mostrar no formulario as silglas das moedas

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 // Coloque aqui suas actions
 // export const addRegister = (value) => ({ type: 'ADD_REGISTER', data: value });
 
-const loginAction = (email) => ({ type: 'saveEmail', email });
+export const loginAction = (email) => ({ type: 'saveEmail', email });
 
-export default loginAction;
+export const getInApi = () => (dispatch) => fetch('https://economia.awesomeapi.com.br/json/all')
+  .then((response) => response.json())
+  .then((response) => dispatch({
+    type: 'GET_CURRENCY',
+    payload: response }))
+  .catch((error) => dispatch({
+    type: 'FAILED_REQUEST',
+    payload: error }));
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,97 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginAction, getInApi } from '../actions';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      valor: 0,
+      descricao: 'Hot Dog',
+      moeda: 'USD',
+      'método de pagamento': undefined,
+      tag: 'Alimentação',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    const { fetchFiltered } = this.props;
+    fetchFiltered();
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleClick(event) {
+    event.preventDefault();
+    const { entradas, cotacoes, myDispatch, fetchFiltered } = this.props;
+    await fetchFiltered();
+    const { valor, descricao, 'método de pagamento': pagamento, moeda, tag } = this.state;
+    let newId = 0;
+    if (entradas.length > 0) {
+      newId = (entradas[entradas.length - 1].id) + 1;
+    }
+    myDispatch({
+      type: 'ADD_EXPENSE',
+      payload: {
+        id: newId,
+        value: valor,
+        currency: moeda,
+        method: pagamento,
+        tag,
+        description: descricao,
+        exchangeRates: cotacoes,
+      },
+    });
+  }
+
   render() {
+    const { moeda, 'método de pagamento': pagamento, tag } = this.state;
+    const { moedas } = this.props;
     return (
-      <form>
+      <form onSubmit={ this.handleClick }>
         <label htmlFor="valor">
           Valor
-          <input id="valor" name="valor" type="number" />
+          <input id="valor" name="valor" type="number" onChange={ this.handleChange } />
         </label>
         <label htmlFor="descricao">
           Descrição
-          <textarea id="descricao" name="descricao" />
+          <textarea id="descricao" name="descricao" onChange={ this.handleChange } />
         </label>
         <label htmlFor="moeda">
           Moeda
-          <select id="moeda" name="moeda">
-            <option> </option>
+          <select id="moeda" name="moeda" value={ moeda } onChange={ this.handleChange }>
+            { Array.isArray(moedas) ? (moedas.map((entry) => (
+              <option key={ entry } value={ entry }>{ entry }</option>
+            ))) : '' }
           </select>
         </label>
         <label htmlFor="método de pagamento">
           Método de pagamento
-          <select id="método de pagamento" name="método de pagamento">
-            <option value="dinheiro">Dinheiro</option>
-            <option value="credito">Cartão de crédito</option>
-            <option value="debito">Cartão de débito</option>
+          <select
+            id="método de pagamento"
+            name="método de pagamento"
+            value={ pagamento }
+            onChange={ this.handleChange }
+          >
+            <option hidden={ Boolean }>-</option>
+            <option key="dinheiro" value="dinheiro">Dinheiro</option>
+            <option key="credito" value="credito">Cartão de crédito</option>
+            <option key="debito" value="debito">Cartão de débito</option>
           </select>
         </label>
         <label htmlFor="tag">
           Tag
-          <select id="tag" name="tag">
+          <select id="tag" name="tag" value={ tag } onChange={ this.handleChange }>
+            <option hidden={ Boolean }>-</option>
             <option value="alimentação">Alimentação</option>
             <option value="lazer">Lazer</option>
             <option value="trabalho">Trabalho</option>
@@ -42,4 +105,29 @@ class Form extends React.Component {
   }
 }
 
-export default Form;
+const mapStateToProps = (state) => ({
+  moedas: state.wallet.currencies[0],
+  cotacoes: state.wallet.currencies[1],
+  entradas: state.wallet.expenses,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  myDispatch: (state) => dispatch(loginAction(state)),
+  fetchFiltered: () => dispatch(getInApi()),
+});
+
+Form.propTypes = {
+  cotacoes: PropTypes.objectOf(PropTypes.any),
+  entradas: PropTypes.arrayOf(PropTypes.any),
+  moedas: PropTypes.arrayOf(PropTypes.any),
+  myDispatch: PropTypes.func.isRequired,
+  fetchFiltered: PropTypes.func.isRequired,
+};
+
+Form.defaultProps = {
+  cotacoes: {},
+  moedas: [],
+  entradas: [],
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
