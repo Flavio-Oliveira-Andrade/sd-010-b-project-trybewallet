028c45e4fc993dfef3084aeca1b5c5a23236b589
Adão Benites <adaobjr@gmail.com>
2021-06-10 22:06:59 -0400

    refactor: :recycle: Function Sum Expenses - Requirement 9

--- src/functions/index.js
+++ src/functions/index.js
@@ -2,24 +2,9 @@
 // ---------------------------------------------------------------------------------------------
 // FormsWallet
 export function sumExpenses(expenses) {
-  const expenseResume = expenses.map(({ value, currency, exchangeRates }) => {
-    const exchangeCoins = Object.values(exchangeRates);
-    const valueNumber = parseFloat(value);
-    const exchangeAsk = parseFloat(exchangeCoins
-      .find((coin) => (coin.code === currency)).ask);
-    const valueInBRL = valueNumber * exchangeAsk;
-    const result = {
-      valueNumber,
-      currency,
-      exchangeAsk,
-      valueInBRL,
-    };
-    return result;
-  });
-  const totalExpenses = expenseResume
-    .reduce((acc, currValue) => acc + currValue.valueInBRL, 0).toFixed(2);
-  // console.log([...expenseResume]);
-  // console.log([totalExpenses]);
+  const listExpenses = expenses
+    .map(({ value, currency, exchangeRates }) => exchangeRates[currency].ask * value);
+  const totalExpenses = listExpenses.reduce((acc, value) => acc + value, 0).toFixed(2);
   return totalExpenses;
 }
 // ---------------------------------------------------------------------------------------------
