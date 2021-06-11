7ffce0f074b3eebba31dfdc6b0fddf2edd2976cb
Pedro Toselli <pedrotrybe@gmail.com>
2021-06-10 23:28:20 -0300

    Requisito 5 - refactored

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,61 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import { useSelector } from 'react-redux';
 
 class Wallet extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dispenses: 0,
-      currency: 'BRL',
+      total: 0,
+      cambioAtual: 0,
     };
   }
 
   render() {
     const { email } = this.props;
-    const { dispenses, currency } = this.state;
+    const { total, cambioAtual } = this.state;
     return (
-      <>
+      <div>
         <header>
-          <p data-testid="email-field">{`Email: ${email}`}</p>
-          <p data-testid="total-field">{`Dispensa total: ${dispenses}`}</p>
-          <p data-testid="header-currency-field">{ currency }</p>
+          <h3 data-testid="email-field">
+            {' '}
+            Email:
+            { email }
+          </h3>
+          <h3 data-testid="total-field">
+            Total Despesas:
+            {total}
+          </h3>
+          <h3 data-testid="header-currency-field">
+            {' '}
+            Câmbio Atual BRL:
+            {cambioAtual}
+          </h3>
         </header>
-        <form>
-          <label htmlFor="value">
-            Valor
-            <input type="text" name="value" />
-          </label>
-          <label htmlFor="description">
-            Descrição
-            <input type="text" name="description" />
-          </label>
-          <label htmlFor="currency">
-            Moeda
-            <select role="combobox" name="currency"> </select>
-          </label>
-          <label htmlFor="payment">
-            Método de pagamento
-            <select role="combobox" name="payment">
-              <option value="money">Dinheiro</option>
-              <option value="credit-card">Cartão de crédito</option>
-              <option value="debit-card">Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="currency">
-            Tag
-            <select role="combobox" name="currency">
-              <option value="alimentacao">Alimentação</option>
-              <option value="lazer">Lazer</option>
-              <option value="trabalho">Trabalho</option>
-              <option value="transporte">Transporte</option>
-              <option value="saude">Saúde</option>
-            </select>
-          </label>
-        </form>
-      </>
+      </div>
     );
   }
 }
