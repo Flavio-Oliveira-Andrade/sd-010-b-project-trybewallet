ac494f56c23f55b82189f7a4fc9d36c079186655
Lara Capila <laracapilak@gmail.com>
2021-06-11 18:44:26 -0300

    requirement - 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/carteira" component={ Header } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
