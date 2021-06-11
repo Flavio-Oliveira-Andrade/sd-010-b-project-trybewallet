84d156fbee58b04acd162bf7863b2862d8c0c305
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-11 02:00:38 -0300

    criando a action selectCurrencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
 // Coloque aqui suas actions
 export const USER_EMAIL = 'USER_EMAIL';
+export const SELECT_CURRENCIES = 'SELECT_CURRENCIES';
 export const loginUser = (email) => ({ type: USER_EMAIL, email });
+export const selectCurrencies = (currencies) => ({ type: SELECT_CURRENCIES, currencies });
