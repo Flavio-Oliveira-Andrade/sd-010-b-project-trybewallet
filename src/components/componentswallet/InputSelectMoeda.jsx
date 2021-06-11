fc1f07fe9693fcf0814585ad61cb7c885f689a83
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-10 22:06:49 -0300

    Cria as options e sincroniza a chamada da api com a do app

--- src/components/componentswallet/InputSelectMoeda.jsx
+++ src/components/componentswallet/InputSelectMoeda.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import requestApi from '../../services/api';
+import getNewId from '../../services/idServices';
 
 function InputSelectMoeda({
   labelDescription = 'Descrição da label',
 }) {
+  const [moedas, setMoedas] = useState([]);
+  // console.log(moedas);
+
+  useEffect(() => {
+    async function getMoedas() {
+      const apiMoedas = await requestApi();
+      setMoedas(Object.values(apiMoedas));
+    }
+    getMoedas();
+  }, []);
+
   return (
     <div>
       <label htmlFor="select-id">
         {labelDescription}
         <select name="moeda" id="select-id">
-          <option>Moeda</option>
+          {moedas.map((item) => <option key={ getNewId() }>{item.code}</option>)}
         </select>
       </label>
     </div>
