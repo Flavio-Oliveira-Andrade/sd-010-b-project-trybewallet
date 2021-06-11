faf17cce7a5545e1cbb07dbdb974226bad40657c
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-11 02:01:17 -0300

    criando a action userExpenses

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 // Coloque aqui suas actions
 export const USER_EMAIL = 'USER_EMAIL';
 export const SELECT_CURRENCIES = 'SELECT_CURRENCIES';
+export const USER_EXPENSES = 'USER_EXPENSES';
 export const loginUser = (email) => ({ type: USER_EMAIL, email });
 export const selectCurrencies = (currencies) => ({ type: SELECT_CURRENCIES, currencies });
+export const userExpenses = (expenses) => ({ type: USER_EXPENSES, expenses });
