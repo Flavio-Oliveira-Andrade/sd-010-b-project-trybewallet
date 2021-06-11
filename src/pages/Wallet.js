994bc1defc87e3ae49b81f357922db3711de5495
Pedro Toselli <pedrotrybe@gmail.com>
2021-06-10 23:44:28 -0300

    Requsito 5 refactored

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,39 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+// import { useSelector } from 'react-redux';
 
 class Wallet extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      total: 0,
-      cambioAtual: 0,
+      dispenses: 0,
+      currency: 'BRL',
     };
   }
 
   render() {
     const { email } = this.props;
-    const { total, cambioAtual } = this.state;
+    const { dispenses, currency } = this.state;
     return (
-      <div>
+      <>
         <header>
-          <h3 data-testid="email-field">
-            {' '}
-            Email:
-            { email }
-          </h3>
-          <h3 data-testid="total-field">
-            Total Despesas:
-            {total}
-          </h3>
-          <h3 data-testid="header-currency-field">
-            {' '}
-            Câmbio Atual BRL:
-            {cambioAtual}
-          </h3>
+          <h2 data-testid="email-field">{ email }</h2>
+          <h2 data-testid="total-field">{ dispenses }</h2>
+          <h2 data-testid="header-currency-field">{ currency }</h2>
         </header>
-      </div>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="text" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" name="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select role="combobox" name="currency"> </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select role="combobox" name="payment">
+              <option value="money">Dinheiro</option>
+              <option value="credit-card">Cartão de crédito</option>
+              <option value="debit-card">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="currency">
+            Tag
+            <select role="combobox" name="currency">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
@@ -41,7 +63,7 @@ class Wallet extends React.Component {
 function mapStateToProps(state) {
   console.log(state);
   return {
-    email: state.user.user.email,
+    email: state.user.email,
   };
 }
 
