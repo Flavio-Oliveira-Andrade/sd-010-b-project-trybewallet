40d62ed73f0cd3db5f7c3a8d3078494d13d0e98d
isTorres <ismaeltorres612@gmail.com>
2021-06-10 21:45:55 -0300

    add api acess and filter currencies 'USDT'

--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -9,3 +9,14 @@ export const actionName = (payload) => ({
   type: GET_TOTAL,
   payload,
 });
+
+export const actionApi = (results) => ({
+  type: GET_API,
+  payload: results,
+});
+
+export const requestApi = () => async (dispatch) => {
+  const result = await getAPI();
+  const dale = Object.values(result).filter((e) => e.codein !== 'BRLT');
+  dispatch(actionApi(dale));
+};
