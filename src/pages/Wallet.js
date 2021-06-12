e12dd090242d0ca43e3d4e408ddc4f8dcaf40185
Dieghonm <dieghonm@gmail.com>
2021-06-12 16:34:59 -0300

    Requisitos 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import './Wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { lerEmail } = this.props;
+    return (
+      <header>
+        <div>TrybeWallet</div>
+        <div className="flex">
+          <p>Email : </p>
+          <p data-testid="email-field">{lerEmail}</p>
+        </div>
+        <div className="flex">
+          <p>Total : </p>
+          <p data-testid="total-field">0</p>
+        </div>
+        <div className="flex">
+          <p>Moeda : </p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </header>
+
+    );
   }
 }
 
+Wallet.propTypes = {
+  lerEmail: PropTypes.string,
+}.isRequired;
+
 const mapStateToProps = (state) => ({
-  lerpage: state.page,
+  lerEmail: state.email,
 });
 
 export default connect(mapStateToProps, null)(Wallet);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,6 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const INITIAL_USUARIO = {
-  email: '',
+  email: 'state inicial eser email apagar depois',
 };
 
 function reducerUsuario(state = INITIAL_USUARIO, action) {
