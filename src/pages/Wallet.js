52315b54e8df1edcdabeb7fdce59e03344f2ea39
brunobastosfer <brunobastos@edu.unifor.br>
2021-06-11 23:58:48 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,14 +3,46 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 0,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({ value });
+  }
+
   render() {
     const { email } = this.props;
+    const { value } = this.state;
     return (
-      <div>
-        Olá
-        {' '}
-        {email}
-      </div>
+      <header>
+        <p data-testid="email-field">
+          Olá
+          {' '}
+          {email}
+        </p>
+        <form>
+          <label htmlFor="total-spent">
+            Total de gastos
+            <input
+              data-testid="total-field"
+              onChange={ this.handleChange }
+              id="total-spent"
+              type="number"
+              value={ value }
+            />
+          </label>
+          <select>
+            <option>BRL</option>
+          </select>
+        </form>
+      </header>
     );
   }
 }
