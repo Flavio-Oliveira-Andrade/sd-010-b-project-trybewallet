0e262398f49c046378d9a427978196bd6c149fa0
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-12 19:02:07 -0300

    configurando conecxão com a API

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,9 @@ export const isFetchApi = () => ({ type: IS_FETCH });
 // export const getCurrencies = () => ({ type: SELECT_CURRENCIES });
 export const selectCurrencies = (currencies) => ({ type: GET_CURRENCIES, currencies });
 // export const userExpenses = (expenses) => ({ type: USER_EXPENSES, expenses });
+export const fetchApi = () => (dispatch) => {
+  dispatch(isFetchApi());
+  fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((currencies) => dispatch(selectCurrencies(currencies)));
+};
