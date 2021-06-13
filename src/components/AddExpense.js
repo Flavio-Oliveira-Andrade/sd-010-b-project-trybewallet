acd36e61ee44a5aa39ee6b2492fdc71cc2c39f3d
André Arnoni <andre_arnoni@yahoo.com.br>
2021-06-13 20:11:01 -0300

    executado requisito 9

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -10,15 +10,15 @@ class AddExpense extends Component {
     return (
       <table>
         <tr>
-          <td>Descrição</td>
-          <td>Tag</td>
-          <td>Método de Pagamento</td>
-          <td>Valor</td>
-          <td>Moeda</td>
-          <td>Câmbio Utilizado</td>
-          <td>Valor Convertido</td>
-          <td>Moeda de Conversão</td>
-          <td>Editar/Excluir</td>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de Pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio Utilizado</th>
+          <th>Valor Convertido</th>
+          <th>Moeda de Conversão</th>
+          <th>Editar/Excluir</th>
         </tr>
         { list.map(({
           description,
