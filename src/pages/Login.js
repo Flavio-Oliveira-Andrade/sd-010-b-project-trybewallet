6e46f7ce24ef9b82a673df1a3557941858d6a793
esionascimento <esio.392@protonmail.com>
2021-06-13 02:32:21 -0400

    concluido req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Login extends React.Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      valido: false,
+      email: '',
+      password: '',
+      validEmail: false,
+      validPassword: false,
+      disabled: true,
     };
-  } */
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleDisabled = this.handleDisabled.bind(this);
+  }
 
-  validarEmail(e) {
-    e.preventDefault();
+  handleDisabled() {
+    const { validEmail, validPassword, disabled } = this.state;
+    console.log(validEmail);
+    console.log(validPassword);
+    console.log(disabled);
+    if (validEmail === true && validPassword === true) {
+      this.setState({ disabled: false });
+    }
+  }
+
+  handleChangePassword(e) {
+    const qtdPassword = e.target.value.length;
+    const passwordLength = 4;
+    if (qtdPassword > passwordLength) {
+      this.setState({
+        password: e.target.value,
+        validPassword: true,
+      });
+      this.handleDisabled();
+    }
+  }
+
+  handleChangeEmail(e) {
+    if (e.target.value.match(/((\w+)@(\w+)\.(\w+))/i)) {
+      this.setState({
+        email: e.target.value,
+        validEmail: true,
+      });
+      this.handleDisabled();
+    } else {
+      this.setState({
+        email: '',
+        validEmail: false,
+      });
+    }
   }
 
   render() {
-    /* const { valido } = this.state; */
+    const { email, password, disabled } = this.state;
+    console.log(email);
+    console.log(password);
     return (
       <>
         <div>Login</div>
-        <input data-testid="email-input" />
-        <input data-testid="password-input" />
-        <button type="button" onClick={ this.validarEmail }>Entrar</button>
+        <input
+          data-testid="email-input"
+          name="email"
+          onChange={ this.handleChangeEmail }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          onChange={ this.handleChangePassword }
+        />
+        <button
+          disabled={ disabled }
+          type="button"
+          onClick={ this.validarEmail }
+        >
+          Entrar
+        </button>
       </>
     );
   }
 }
 
-export default Login;
+export default connect()(Login);
