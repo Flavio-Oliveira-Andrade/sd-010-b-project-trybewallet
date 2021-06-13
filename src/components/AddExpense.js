e4c256fcd79339e27bbbad5a21596cbcffa5bc9c
André Arnoni <andre_arnoni@yahoo.com.br>
2021-06-13 20:22:29 -0300

    executado requisito 9

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -12,12 +12,12 @@ class AddExpense extends Component {
         <tr>
           <th>Descrição</th>
           <th>Tag</th>
-          <th>Método de Pagamento</th>
+          <th>Método de pagamento</th>
           <th>Valor</th>
           <th>Moeda</th>
-          <th>Câmbio Utilizado</th>
-          <th>Valor Convertido</th>
-          <th>Moeda de Conversão</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
         { list.map(({
