defc9e557faee52fb16ec0156e8337525b7ab8d8
Dieghonm <dieghonm@gmail.com>
2021-06-12 21:26:52 -0300

    Requisitos 7, tentativa 1

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,4 +1,10 @@
-export default async function getMoedas() {
+export async function getMoedas() {
+  return fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((fetchReturn) => fetchReturn.json())
+    .catch((error) => error);
+}
+
+export async function morreLint() {
   return fetch('https://economia.awesomeapi.com.br/json/all')
     .then((fetchReturn) => fetchReturn.json())
     .catch((error) => error);
