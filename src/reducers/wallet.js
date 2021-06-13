f2b50dd77fc346b7f07792946ffeddead3ea42d8
DennisMarcelo <dennismarcelo15@gmail.com>
2021-06-13 11:42:12 -0300

    configuração final do botão deletExpense, reducer configurado para deletar expense

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { WALLET_SPEND } from '../actions';
+import { WALLET_SPEND, DELET_EXPENSE } from '../actions';
 
 const userDefaul = {
   currencies: [],
@@ -6,11 +6,17 @@ const userDefaul = {
 };
 
 function walletReducer(state = userDefaul, action) {
+  const newArrayExpenses = state.expenses.filter(({ id }) => id !== action.id);
   switch (action.type) {
   case WALLET_SPEND:
     return {
       ...state,
       expenses: [...state.expenses, action.newSpend],
+    };
+  case DELET_EXPENSE:
+    return {
+      ...state,
+      expenses: newArrayExpenses,
     };
   default:
     return state;
