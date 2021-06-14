be50a0dd4ad74ee21f11c6fce71c195d2f14174c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-14 10:31:44 -0300

    Cria um helper para armazenar state inicial

---
+++ src/helpers/initialState.js
@@ -0,0 +1,12 @@
+// import { getNewId } from '../services/idServices';
+
+const objExpenses = {
+  id: 0,
+  description: '',
+  value: 0,
+  currency: 'BRL',
+  method: 'Dinheiro',
+  tag: 'Alimentação',
+};
+
+export default objExpenses;
