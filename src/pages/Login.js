0a66d04ab41b83f90222780fd56bd7767b66d5d8
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-06-13 21:36:37 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Digite seu email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </section>
+    );
   }
 }
 
