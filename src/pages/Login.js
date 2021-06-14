be9bfe7b8ea8e1ed60483f9125eb3e83c9618c6a
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-06-14 10:58:47 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <section>
+          <input
+            type="email"
+            placeholder="email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            data-testid="password-input"
+          />
+          <button type="button">Entrar</button>
+        </section>
+      </div>
+    );
   }
 }
 
