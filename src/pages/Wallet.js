2604b3fc8c0779f04ee7e4dc5683651cadf0dc0a
Mariana Savoldi <marianasavodi123@gmail.com>
2021-06-14 16:58:56 -0300

    Arrumando o header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import FormWallet from '../components/FormWallet';
-import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
+    const { email, total } = this.props;
     return (
       <div>
-        <Header />
+        <header>
+          <h3 data-testid="email-field">{ email }</h3>
+          <p data-testid="total-field">{ total }</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
         <FormWallet />
       </div>
     );
@@ -18,5 +23,14 @@ const mapStateToProps = (state) => ({
   email: state.user.email,
   total: state.wallet.total,
 });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  total: PropTypes.number,
+};
+
+Wallet.defaultProps = {
+  total: 0,
+};
 
 export default connect(mapStateToProps, null)(Wallet);
