8c6f6d5b52ef39e1ad157b121900c03648ae398b
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-06-14 23:47:11 -0300

    tentando req1, configurando redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
+import store from './Store/index';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <Switch>
+          <Route path="/" component={ Login } />
+        </Switch>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    };
+  }
+
+  handleSubmit(target) {
+    const targetResult = target;
+    console.log(targetResult);
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form onSubmit={ () => this.handleSubmit() }>
+        <label htmlFor="email">
+          <input
+            data-testid="email-input"
+            name="email"
+            placeholder="Email"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            data-testid="password-input"
+            name="password"
+            placeholder="Senha"
+          />
+        </label>
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
