00ce67ce10a6e05d9bc8d5a0b91bb80e7f58eee1
Bruna Campos <92brunacampos@gmail.com>
2021-06-14 21:37:41 -0300

    Requisito 6

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,60 +1,60 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { render } from 'react-dom';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import { render } from 'react-dom';
 
 class Form extends React.Component {
-	constructor() {
-		super();
-	}
+  // constructor() {
+  // super();
+  // }
+  //
+  // this.state = {
+  // value: 0,
+  // description: '',
+  // currency: 'USD',
+  // method: 'Dinheiro',
+  // tag: 'Alimentação',
+  // };
 
-	this.state = {
-		valor: '',
-		description: '',
-		currency: '',
-		method: '',
-		despesas: '',
-	};
-};
+  render() {
+    return (
+      <form>
+        <label htmlFor="value">
+          Valor
+          <input type="number" id="value" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <input type="text" id="description" onChange={ this.handleChange } />
+        </label>
+        <label htmlFor="currency">
+          Moeda
+          <select id="currency" onChange={ this.handleChange }>
+            <option value="initial">0</option>
+          </select>
+        </label>
 
-render() {
+        <label htmlFor="method">
+          Método de Pagamento
+          <select id="method" onChange={ this.handleChange }>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de Débito">Cartão de Débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag
+          <select id="tag" onChange={ this.handleChange }>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
 
-	return (
-		<form>
-			<label htmlFor="value">
-				Valor
-				<input
-					type="number"
-					id="value"
-					onChange={ this.handleChange}
-				/>
-			</label>
-			<label htmlFor="description">
-				Descrição
-				<input
-					type="text"
-					id="description"
-					onChange={ this.handleChange}
-				/>
-			</label>
-			<label htmlFor="currency">
-				Moeda
-				<select
-					id="currency"
-					onChange={ this.handleChange}
-				>
-				</select>
-			</label>
-
-			<label htmlFor="method">
-				Método de Pagamento
-				<select
-					type=""
-					id=""
-					onChange={ this.handleChange}
-				>
-				</select>
-			</label>
-		</form>
-	)
-};
+export default Form;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Form />
+      </>
     );
   }
 }
