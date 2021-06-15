3a4e24d3c40664de8e9a5b0e80965fe956efc6d1
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-14 22:15:07 -0300

    Refatora Header

--- src/components/componentswallet/Header.jsx
+++ src/components/componentswallet/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Header({ children = null }) {
+function Header() {
+  const emailUser = useSelector((state) => state.user.email);
+  const expenses = useSelector((state) => state.wallet.expenses);
+
+  console.log(expenses);
+
+  const moeda = expenses.map(({ currency }) => currency);
+
   return (
     <div>
-      {children}
+      <span data-testid="email-field">{`Email: ${emailUser}`}</span>
+      <span data-testid="total-field">{`Total das despesas: ${expenses.value}`}</span>
+      <span
+        data-testid="header-currency-field"
+      >
+        {`Moeda: ${moeda}`}
+      </span>
     </div>
   );
 }
