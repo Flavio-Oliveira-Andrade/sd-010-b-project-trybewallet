39359b62ea7b1cc3bc26f9d80e58e1018f0dfd2e
Luiz Reina <luizreeina@gmail.com>
2021-06-15 12:41:38 -0300

    requirements 5 to 10

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import WalletExpenseIncluder from '../components/WalletExpenseIncluder';
+import WalletExpenseTable from '../components/WalletExpenseTable';
+import WalletHeader from '../components/WalletHeader';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+        <WalletHeader />
+        <WalletExpenseIncluder />
+        <WalletExpenseTable />
+      </>
+    );
   }
 }
 
