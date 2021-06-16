bb06aae5adbb168639c874a5cc3a5bcfe3e08b81
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-16 11:28:47 -0300

    valor inicial do hearder

--- src/components/componentswallet/Header.jsx
+++ src/components/componentswallet/Header.jsx
@@ -8,11 +8,12 @@ function Header() {
   console.log(expenses);
 
   const moeda = expenses.map(({ currency }) => currency);
+  const valor = expenses.map(({ value }) => value);
 
   return (
     <div>
       <span data-testid="email-field">{`Email: ${emailUser}`}</span>
-      <span data-testid="total-field">{`Total das despesas: ${expenses.value}`}</span>
+      <span data-testid="total-field">{`Total das despesas: ${valor}`}</span>
       <span
         data-testid="header-currency-field"
       >
