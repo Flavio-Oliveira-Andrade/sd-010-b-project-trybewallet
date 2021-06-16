50c7a11708f5fe150f2289e4d192db3faeb73777
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-06-16 17:02:12 -0300

    criando os arquivos redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const LOGIN_USER = 'LOGIN_USER';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+
+export const loginUser = (email) => ({
+  type: LOGIN_USER,
+  email,
+});
+
+export const addExpense = (expense) => ({
+  type: ADD_EXPENSE,
+  expense,
+});
