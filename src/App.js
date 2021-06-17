3c7ba24048aea2b4091d680091c29c6fec256d65
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-06-17 13:13:28 -0300

    eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
-import './App.css';
 import Wallet from './pages/Wallet';
 
 class App extends React.Component {
