30c44ee0d8a677f8ae12f395cf639ea6263e81f5
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 10:49:04 -0300

    Cria a function e captura o array da store do redux

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Table() {
+  const expenses = useSelector((state) => state.wallet.expenses);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Table;
