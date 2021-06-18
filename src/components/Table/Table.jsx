0ae719b9e6913bde2ea784a320590af30b0d6f96
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 10:53:56 -0300

    Cria o cabeçalho da tabela e chama a função de adição de despesas

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -28,9 +28,20 @@ function Table() {
   }
   return (
     <div>
-      
+      <tr>
+        <th value="Descrição">Descrição</th>
+        <th value="Tag">Tag</th>
+        <th value="Método de pagamento">Método de pagamento</th>
+        <th value="Valor">Valor</th>
+        <th value="Moeda">Moeda</th>
+        <th value="Câmbio utilizado">Câmbio utilizado</th>
+        <th value="Valor convertido">Valor convertido</th>
+        <th value="Moeda de conversão">Moeda de conversão</th>
+        <th value="Editar/Excluir">Editar/Excluir</th>
+      </tr>
+      { tableExpenses() }
     </div>
-  )
+  );
 }
 
 export default Table;
