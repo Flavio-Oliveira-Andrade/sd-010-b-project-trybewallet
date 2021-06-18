9fb433317036740bf2fe42b3115e95ab1a0334b7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 11:52:21 -0300

    Remove a função que cria o conteudo do componente Table e adiciona no componente Contents

--- src/components/Table/ContentsTable.jsx
+++ src/components/Table/ContentsTable.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 function ContentsTable() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const expenses = useSelector((state) => state.wallet.expenses);
+  const dispatch = useDispatch();
+
+  const valueExpenses = expenses.map((item) => {
+    const exchangeRate = (parseFloat(item.exchangeRates[item.currency].ask)).toFixed(2);
+    return (
+      <tr key={ getNewId() }>
+        <td>{ item.description }</td>
+        <td>{item.tag}</td>
+        <td>{ item.method }</td>
+        <td>{item.value}</td>
+        <td>{ item.exchangeRates[item.currency].name.split('/')[0] }</td>
+        <td>{ exchangeRate }</td>
+        <td>
+          {(parseFloat(item.value * item.exchangeRates[item.currency].ask))
+            .toFixed(2)}
+        </td>
+        <td>Real</td>
+        <td>
+          <button
+            data-testid="delete-btn"
+            type="button"
+          >
+            Deletar
+          </button>
+        </td>
+      </tr>
+    );
+  });
+  return valueExpenses;
 }
 
-export default ContentsTable
+export default ContentsTable;
