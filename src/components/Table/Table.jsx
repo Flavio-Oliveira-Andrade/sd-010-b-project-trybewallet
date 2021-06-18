9d53e00e7df47aa4640cd9a2e240f9d72b39240d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 11:56:31 -0300

    Renderiza componentes Header e Contents

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -30,18 +30,8 @@ function Table() {
   }
   return (
     <div>
-      <tr>
-        <th value="Descrição">Descrição</th>
-        <th value="Tag">Tag</th>
-        <th value="Método de pagamento">Método de pagamento</th>
-        <th value="Valor">Valor</th>
-        <th value="Moeda">Moeda</th>
-        <th value="Câmbio utilizado">Câmbio utilizado</th>
-        <th value="Valor convertido">Valor convertido</th>
-        <th value="Moeda de conversão">Moeda de conversão</th>
-        <th value="Editar/Excluir">Editar/Excluir</th>
-      </tr>
-      { tableExpenses() }
+      <HeaderTable />
+      <ContentsTable />
     </div>
   );
 }
