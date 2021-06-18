5e5b1122c2f7098dbb1da428214b6e7c4e29b159
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 10:51:22 -0300

    Cria a função que adiciona os registros da despesas na tabela

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,6 +3,28 @@ import { useSelector } from 'react-redux';
 
 function Table() {
   const expenses = useSelector((state) => state.wallet.expenses);
+
+  function tableExpenses() {
+    const valueExpenses = expenses.map((item) => {
+      const exchangeRate = (parseFloat(item.exchangeRates[item.currency].ask)).toFixed(2);
+      return (
+        <tr key={ getNewId() }>
+          <td>{ item.description }</td>
+          <td>{item.tag}</td>
+          <td>{ item.method }</td>
+          <td>{item.value}</td>
+          <td>{ item.exchangeRates[item.currency].name.split('/')[0] }</td>
+          <td>{ exchangeRate }</td>
+          <td>
+            {(parseFloat(item.value * item.exchangeRates[item.currency].ask))
+              .toFixed(2)}
+          </td>
+          <td>Real</td>
+        </tr>
+      );
+    });
+    return valueExpenses;
+  }
   return (
     <div>
       
