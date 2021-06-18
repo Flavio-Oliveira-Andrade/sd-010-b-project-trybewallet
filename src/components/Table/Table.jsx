3c22ded509aa1dd09fb67849f467be1b7fec090b
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-18 16:16:23 -0300

    Refatora a table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,34 +5,14 @@ import ContentsTable from './ContentsTable';
 // As conversões de moedas foram baseadas na ajuda dada pelo Marcos Leandro
 
 function Table() {
-  const expenses = useSelector((state) => state.wallet.expenses);
-
-  function tableExpenses() {
-    const valueExpenses = expenses.map((item) => {
-      const exchangeRate = (parseFloat(item.exchangeRates[item.currency].ask)).toFixed(2);
-      return (
-        <tr key={ getNewId() }>
-          <td>{ item.description }</td>
-          <td>{item.tag}</td>
-          <td>{ item.method }</td>
-          <td>{item.value}</td>
-          <td>{ item.exchangeRates[item.currency].name.split('/')[0] }</td>
-          <td>{ exchangeRate }</td>
-          <td>
-            {(parseFloat(item.value * item.exchangeRates[item.currency].ask))
-              .toFixed(2)}
-          </td>
-          <td>Real</td>
-        </tr>
-      );
-    });
-    return valueExpenses;
-  }
   return (
-    <div>
-      <HeaderTable />
-      <ContentsTable />
-    </div>
+    <table>
+      <tbody>
+        <HeaderTable />
+        <ContentsTable />
+      </tbody>
+    </table>
+
   );
 }
 
