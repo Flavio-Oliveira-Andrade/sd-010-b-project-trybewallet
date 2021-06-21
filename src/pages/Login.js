a4ad374075a2b24e0210a7b001255df8bb7da4b7
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-06-21 15:53:56 -0300

    comecando do zero

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label>
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="password"
+          />
+          <button
+          type="button"
+          >
+            Entrar
+          </button>
+        </label>
+      </form>
+    )
   }
 }
 
