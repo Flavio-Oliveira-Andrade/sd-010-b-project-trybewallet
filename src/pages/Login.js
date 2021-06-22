412298d24d00f2000fb378e3c90a1190d0a88f4d
Aderson Santos <assaderson@gmail.com>
2021-06-22 13:24:12 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  /* constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.validacao = this.validacao.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  } */
+
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <div>Tela de Login</div>
+        <form>
+          <label htmlFor="email">
+            E-mail:
+            <input type="email" name="email" data-testid="email-input" required />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input type="password" name="password" data-testid="password-input" />
+          </label>
+          <button type="button">
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
