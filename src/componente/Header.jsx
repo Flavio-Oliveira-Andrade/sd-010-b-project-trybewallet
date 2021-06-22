c089a132f83a062a1b984bf6c0cfdcd39cac7e1e
alinemelry <aline.melryaa@gmail.com>
2021-06-22 10:00:48 -0400

    correçao data-testid no header

--- src/componente/Header.jsx
+++ src/componente/Header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
     return (
       <main>
         <header>
-          <section className="user-info" data-testid="emailfield">
+          <section className="user-info" data-testid="email-field">
             Usuario:
 
             {email}
