abfc486f4824070066423772b93824ff20afc748
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-06-22 11:18:14 -0300

    requisito 2,3 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { func } from 'prop-types';
+import { Link } from 'react-router-dom';
+import { handleUserLogin as loginAction } from '../actions/index';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      disableBtn: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  validate() {
+    const { email, password, disableBtn } = this.state;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const passwordLength = 6;
+    if (emailRegex.test(email) && password.length >= passwordLength) {
+      this.setState({ disableBtn: false });
+    } else if (!disableBtn) {
+      this.setState({ disableBtn: true });
+    }
+  }
+
+  handleChange(event) {
+    const { value, type } = event.target;
+    this.setState(
+      { [type]: value },
+      () => (this.validate()),
+    );
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { handleUserLogin } = this.props;
+    const { email } = this.state;
+    handleUserLogin(email);
+  }
+
   render() {
+    const { email, password, disableBtn } = this.state; 
+    
     return (
       <form>
-        <div>
+        <label htmlFor="email-input">
           <input
             type="email"
+            id="email-input"
             data-testid="email-input"
             placeholder="email"
+            onChange={ this.handleChange }
+            value={ email }
           />
+         <label htmlFor="password-input">
           <input
-            type="password"
-            data-testid="password-input"
-            placeholder="password"
-          />
+              type="password"
+              id="password-input"
+              data-testid="password-input"
+              placeholder="password"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label> 
+
           <button
-            type="button"
+            type="submit"
+            disabled={ disableBtn }
+            onClick={ this.handleSubmit }
           >
-            Entrar
+            <Link to="carteira">Entrar</Link>
           </button>
-        </div>
+        </label>
       </form>
     );
   }
 }
 
-export default Login;
+
+const mapDispatchToProps = (dispatch) => ({
+  handleUserLogin: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = { handleUserLogin: func.isRequired };
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ADD_EXPENSE } from '../actions';
+
+const initialState = {
+  currencies: [],
+  expenses: [],
+};
+
+export default (state = initialState, action) => {
+  const { type, expense } = action;
+  switch (type) {
+  case ADD_EXPENSE:
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
+    };
+  default:
+    return state;
+  }
+};
