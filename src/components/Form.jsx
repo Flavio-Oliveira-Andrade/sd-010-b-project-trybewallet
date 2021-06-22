d34cdd6fb813f206ab8f2767ed0375cdd9722bb9
Lara Capila <laracapilak@gmail.com>
2021-06-22 19:57:58 -0300

    implementação css Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExpenses, fetchApi, fetchApiCoin } from '../actions';
 
+import '../style/form.css';
+
 class Form extends Component {
   constructor(props) {
     super(props);
@@ -45,9 +47,14 @@ class Form extends Component {
   // Tive que trazer os inputs para fora do render pois ele excedeu o limite de linhas
   inputMethod() {
     return (
-      <label htmlFor="payment">
+      <label className="labels-form" htmlFor="payment">
         Método de pagamento
-        <select id="payment" name="method" onChange={ this.handlechange }>
+        <select
+          className="inputs-form"
+          id="payment"
+          name="method"
+          onChange={ this.handlechange }
+        >
           <option>Dinheiro</option>
           <option>Cartão de crédito</option>
           <option>Cartão de débito</option>
@@ -59,9 +66,14 @@ class Form extends Component {
   inputCurrency() {
     const { coinNames } = this.props;
     return (
-      <label htmlFor="currencie">
+      <label className="labels-form" htmlFor="currencie">
         Moeda
-        <select id="currencie" name="currency" onChange={ this.handlechange }>
+        <select
+          className="inputs-form"
+          id="currencie"
+          name="currency"
+          onChange={ this.handlechange }
+        >
           { coinNames.map((currencie) => (
             <option
               value={ currencie }
@@ -77,9 +89,14 @@ class Form extends Component {
 
   inputTag() {
     return (
-      <label htmlFor="tag">
+      <label className="labels-form" htmlFor="tag">
         Tag
-        <select id="tag" name="tag" onChange={ this.handlechange }>
+        <select
+          className="inputs-form"
+          id="tag"
+          name="tag"
+          onChange={ this.handlechange }
+        >
           <option>Alimentação</option>
           <option>Lazer</option>
           <option>Trabalho</option>
@@ -92,23 +109,25 @@ class Form extends Component {
 
   render() {
     return (
-      <form>
-        <label htmlFor="value">
+      <form className="forms">
+        <label className="labels-form" htmlFor="value">
           Valor
           <input
             type="number"
             id="value"
             name="value"
             onChange={ this.handlechange }
+            className="inputs-form"
           />
         </label>
-        <label htmlFor="description">
+        <label className="labels-form" htmlFor="description">
           Descrição
           <input
             type="text"
             id="description"
             name="description"
             onChange={ this.handlechange }
+            className="inputs-form"
           />
         </label>
         { this.inputCurrency() }
