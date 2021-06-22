70524a11a793585f5b1aacf916a55a5686598e39
alinemelry <aline.melryaa@gmail.com>
2021-06-22 10:25:07 -0400

    correçao do isEdit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ export const loginAction = (email) => ({ type: 'LOGIN', email });
 export const addExpense = (expense) => ({ type: 'ADD', expense });
 export const updateCurrencies = (currency) => ({ type: 'UPDATE', currency });
 export const deleteExpense = (expense) => ({ type: 'DELETE', expense });
-export const editExpense = (expense) => ({ type: 'EDIT', expense, isEdit });
+export const editExpense = (expense) => ({ type: 'EDIT', expense, isEdit: null });
 export const setExpenses = (expenses) => ({ type: 'SET_EXPENSES', expenses });
