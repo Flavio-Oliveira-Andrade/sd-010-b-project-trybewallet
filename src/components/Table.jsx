7eebfa1b896a37047ca839194a23375e1d165b7c
RafaMaths <rmathscoach@gmail.com>
2021-06-23 17:37:06 -0300

    update files

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,7 +8,42 @@ export default class Table extends Component {
 
   render() {
     return (
-      <section />
+      <section>
+        <table>
+          <tr>
+            <th>
+              Descrição
+            </th>
+            <th>
+              Tag
+            </th>
+            <th>
+              Método de pagamento
+            </th>
+            <th>
+              Valor
+            </th>
+            <th>
+              Moeda
+            </th>
+            <th>
+              Câmbio
+            </th>
+            <th>
+              Câmbio utilizado
+            </th>
+            <th>
+              Valor convertido
+            </th>
+            <th>
+              Moeda de conversão
+            </th>
+            <th>
+              Editar/Excluir
+            </th>
+          </tr>
+        </table>
+      </section>
     );
   }
 }
