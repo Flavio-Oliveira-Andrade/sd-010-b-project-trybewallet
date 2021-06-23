339a54b440777fc8ddbe34535496c0cf3a559f61
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-06-23 20:08:57 -0300

    Primeiro(s) requisito(s)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      password: '',
+      email: '',
+      disable: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    const { password, email } = this.state;
+    const minLength = 4;
+    if(password.length > minLength && email.includes('.com')){
+      this.setState({ [event.target.name]: event.target.value})
+      this.setState({ disable: false });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disable } = this.state;
+    return (
+    <div className="Login">
+      <form className="formLogin">
+        <input
+        placeholder="email"
+        name="email"
+        data-testid="email-input"
+        onChange={ this.handleChange }
+        />
+        <input
+        placeholder="senha"
+        name="password"
+        data-testid="password-input"
+        onChange={ this.handleChange }
+        />
+        <button
+        type="button"
+        disabled={ disable }
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+    );
   }
 }
 
