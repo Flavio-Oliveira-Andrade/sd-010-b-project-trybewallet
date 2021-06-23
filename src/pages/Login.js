b6a77d00afb584f4177ff501f6a008983c84e220
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-06-23 16:40:29 -0300

    requisito dois(2)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,59 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      login: true,
+    };
+  }
 
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.confirmationCheck(this.state);
+    });
+  }
 
-  validation({ userEmail, userPassword }) {
-    const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    const Email = regex.test(userEmail);
-    const minNumber = 6;
+  confirmationCheck ({ password, email }) {
+    const caract = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const emailTest = caract.test(email);
+    const passwordMin = 6;
 
-    
+    if (email  && password.length >= passwordMin) {
+      this.setState({
+        login: false,
+      });
+    } else {
+      this.setState({
+        login: true,
+      });
+    }
+  }
+
   render() {
+    const { login, password, email } = this.state;
     return (
       <form>
-        <label>
+        <label htmlFor="email">
           Email:
           <input
+            id="email"
+            type=""
+            name="email"
+            onChange={this.handleChange}
             data-testid="email-input"
           />
         </label>
-        <label>
+        <label htmlFor="password">
           Senha:
           <input
+            id="password"
+            name="password"
+            onChange={this.handleChange}
             data-testid="password-input"
           />
         </label>
