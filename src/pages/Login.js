6c764133b22e7d349e3fbac0319b9913c8bfec81
Aderson Santos <assaderson@gmail.com>
2021-06-23 09:46:19 -0300

    Requisito 2 e 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,38 +1,93 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { addUser } from '../actions';
 
 class Login extends React.Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
+      disabled: true,
     };
 
-    this.validacao = this.validacao.bind(this);
+    this.validarFormulario = this.validarFormulario.bind(this);
     this.handleChange = this.handleChange.bind(this);
-  } */
+  }
+
+  validarFormulario() {
+    const { email, password } = this.state;
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const seisCaracteres = 6;
+    // const botao = document.querySelector('button');
+    if ((emailRegex.test(email)) && (password.length > seisCaracteres)) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    if (name === 'email') {
+      this.setState({ email: value }, this.validarFormulario);
+    }
+    if (name === 'password') {
+      this.setState({ password: value }, this.validarFormulario);
+    }
+  }
 
   render() {
+    const { disabled, email } = this.state;
+    const { addUserState } = this.props;
     return (
       <section>
         <div>Tela de Login</div>
         <form>
           <label htmlFor="email">
             E-mail:
-            <input type="email" name="email" data-testid="email-input" required />
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              required
+            />
           </label>
           <label htmlFor="password">
             Senha:
-            <input type="password" name="password" data-testid="password-input" />
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              seisCaracteres="6"
+              onChange={ this.handleChange }
+              required
+            />
           </label>
-          <button type="button">
+        </form>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disabled }
+            onClick={ () => addUserState(email) }
+          >
             Entrar
           </button>
-        </form>
+        </Link>
       </section>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addUserState: (email) => dispatch(addUser(email)),
+});
+
+Login.propTypes = {
+  addUserState: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <section>
+        <div>TrybeWallet</div>
+      </section>
+    );
   }
 }
 
