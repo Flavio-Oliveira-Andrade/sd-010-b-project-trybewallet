1c6d7cf91434b7538da97d202ec039ecb32403ad
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-06-23 04:48:17 -0300

    importa moedas api

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,9 @@
-import { EMAIL_CHECK, PASSWORD_CHECK } from '../actions/index';
+import { EMAIL_CHECK, PASSWORD_CHECK } from '../actions/loginActions';
 
 // Esse reducer será responsável por tratar as informações da pessoa usuária
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_USER_STATE = { email: '', password: '' };
 
-const user = (state = INITIAL_STATE, action) => {
+const user = (state = INITIAL_USER_STATE, action) => {
   switch (action.type) {
   case EMAIL_CHECK:
     return ({
