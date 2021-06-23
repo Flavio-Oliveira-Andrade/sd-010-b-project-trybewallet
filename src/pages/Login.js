bb9ed8a28a9c68b83dc7acc8ac8b3eefe0e72b57
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-06-23 16:31:29 -0300

    Implementado requisito 02.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,58 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleOnChange({ target }) {
+    const { value, type } = target;
+
+    this.setState({
+      [type]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+    const MAGIC_NUMBER = 6;
+
+    const validationEmail = () => {
+      const regexEmail = /\S+@\S+\.\S+/;
+
+      const validation = regexEmail.test(email);
+      return validation;
+    };
+
+    const passwordValidation = password.length >= MAGIC_NUMBER;
+
     return (
       <section>
         <h1>TRYBE</h1>
         <div>
-          <input type="email" data-testid="email-input" />
-          <input type="password" data-testid="password-input" />
-          <button type="submit">Entrar</button>
+          <input
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleOnChange }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleOnChange }
+          />
+          <button
+            type="button"
+            disabled={ !(validationEmail() && passwordValidation) }
+          >
+            Entrar
+          </button>
         </div>
       </section>
     );
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+function user(state = INITIAL_STATE, action) {
+
+}
