bf054f4769e92074dd7546420ca02cb91779f022
Matheus Gabriel Pereira do Couto <70240441+MatheusGabrielCouto@users.noreply.github.com>
2021-06-23 08:42:13 -0400

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import '../App.css'
+
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return(
+      <div className="container">
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <input data-testid="password-input" type="password" placeholder="Senha" required />
+        <button>Entrar</button>
+      </div>
+    );
   }
 }
 
