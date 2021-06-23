66dc3fa69ada7c14a6713053c436b2cd1bbdca51
Matheus Gabriel Pereira do Couto <70240441+MatheusGabrielCouto@users.noreply.github.com>
2021-06-23 09:52:08 -0400

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,8 +11,9 @@ function Login() {
     // Essa parte do reg peguei no stackoverflow https://pt.stackoverflow.com/questions/348854/fun%C3%A7%C3%A3o-em-javascript-para-valida%C3%A7%C3%A3o-de-email-n%C3%A3o-entra-no-else#:~:text=A%20fun%C3%A7%C3%A3o%20replace%20serve%20para,o%20retorno%20de%20uma%20function%20).
     // eslint-disable-next-line
     const reg = /^[\w.\+]+@\w+.\w{2,}(?:.\w{2})?$/;
+    const maxLength = 6
 
-    if (reg.test(email) === true && password.length >= 6) {
+    if (reg.test(email) === true && password.length >= maxLength) {
       setVerify(true);
     } else {
       setVerify(false);
