11288a90aae0ba0e37376d3c2ebac25f8245b480
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-06-23 15:32:26 -0300

    feito requisito um(1)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class Login extends React.Component {
+
+
+  validation({ userEmail, userPassword }) {
+    const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const Email = regex.test(userEmail);
+    const minNumber = 6;
+
+    
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label>
+          Email:
+          <input
+            data-testid="email-input"
+          />
+        </label>
+        <label>
+          Senha:
+          <input
+            data-testid="password-input"
+          />
+        </label>
+        <button>Entrar</button>
+      </form>
+    );
   }
 }
 
