d8fa683bb709ce70513a64e956d64f4be6fd0c46
Lara Capila <laracapilak@gmail.com>
2021-06-23 10:17:06 -0300

    implementação css Table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { dellExpense } from '../actions';
 
+import '../style/table.css';
+
 class Table extends React.Component {
   constructor(props) {
     super(props);
@@ -22,42 +24,48 @@ class Table extends React.Component {
     const { expenses } = this.props;
     return (
       // Relembrar tabelas em html https://www.homehost.com.br/blog/criar-sites/tabela-html/
-      <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        {expenses.map((element, index) => (
-          <tr key={ index }>
-            <td>{element.description}</td>
-            <td>{element.tag}</td>
-            <td>{element.method}</td>
-            <td>{element.value}</td>
-            <td>{element.exchangeRates[element.currency].name.split('/')[0]}</td>
-            <td>{parseFloat(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
-            <td>
-              {parseFloat(element.value
-                * element.exchangeRates[element.currency].ask).toFixed(2)}
-            </td>
-            <td>Real</td>
-            <td>
-              <button
-                type="button"
-                data-testid="delete-btn"
-                onClick={ () => this.dell(index) }
-              >
-                Deletar
-              </button>
-            </td>
+      <table className="container-table">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {expenses.map((element, index) => (
+            <tr key={ index }>
+              <td>{element.description}</td>
+              <td>{element.tag}</td>
+              <td>{element.method}</td>
+              <td>{element.value}</td>
+              <td>{element.exchangeRates[element.currency].name.split('/')[0]}</td>
+              <td>
+                {parseFloat(element.exchangeRates[element.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                {parseFloat(element.value
+                  * element.exchangeRates[element.currency].ask).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button
+                  type="button"
+                  data-testid="delete-btn"
+                  onClick={ () => this.dell(index) }
+                >
+                  Deletar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
