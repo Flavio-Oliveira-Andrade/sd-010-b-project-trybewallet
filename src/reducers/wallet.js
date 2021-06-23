35ea55900d6ad9d7261c43deab849d76a988461c
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-06-23 20:39:36 -0300

    Implementado requisito 05.

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return {
+      email: action.payload.email,
+    };
+  default:
+    return state;
+  }
+}
