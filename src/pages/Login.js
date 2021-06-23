f720143c1dab0a55bdb027354da5561cd1066c78
Aladino Borges <eualadino@gmail.com>
2021-06-23 12:23:14 -0300

    Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,51 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+// import PropTyoes from 'prop-types';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {};
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-form">
+        Login
+        <label htmlFor="email">
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
