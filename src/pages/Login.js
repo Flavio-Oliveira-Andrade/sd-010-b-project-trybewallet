d0ace8c398ccbf1b88849823a1e2b187efde1ae7
Holistic <paulofreire.si@outlook.com>
2021-06-24 06:36:52 -0300

    baby come back, any kind of fool could see...

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          User email:
+          <input type="email" name="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="pwd">
+          Password:
+          <input type="password" name="pwd" data-testid="password-input" />
+        </label>
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,38 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  errors: '',
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'CURRENCY':
+    return {
+      ...state,
+      currencies: [
+        Object.keys(action.payload).filter((entry) => entry !== 'USDT'),
+        action.payload,
+      ],
+    };
+  case 'ADD_EXPENSE':
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload],
+    };
+  case 'REMOVE_EXPENSIVE':
+    return {
+      ...state,
+      expenses: [...state.expenses.filter((entry) => (
+        entry.id !== parseInt(action.payload, 10)))],
+    };
+  case 'FAIL':
+    return {
+      ...state,
+      error: action.payload,
+    };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
