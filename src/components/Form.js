a95cafbd4058067245a5e733f85944177fdca600
Breno3B <brenorm@gmail.com>
2021-06-24 16:55:23 -0300

    Requisito 6: Mudança das labels para funçao

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,63 +14,93 @@ class Form extends Component {
     };
   }
 
+  functionValue() {
+    return (
+      <label htmlFor="value">
+        Valor
+        <input
+          type="text"
+          id="value"
+          data-testid="value-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  functionDescription() {
+    return (
+      <label htmlFor="description">
+        Descrição
+        <input
+          type="text"
+          id="description"
+          data-testid="description-input"
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  functionCurrency() {
+    return (
+      <label htmlFor="currency">
+        Moeda
+        <select
+          id="currency"
+          data-testid="currency-input"
+          onChange={ this.handleChange }
+        >
+          <option value="1">1</option>
+        </select>
+      </label>
+    );
+  }
+
+  functionMethod() {
+    return (
+      <label htmlFor="method">
+        Método de pagamento
+        <select
+          id="method"
+          data-testid="method-input"
+          onChange={ this.handleChange }
+        >
+          <option value="1">Dinheiro</option>
+          <option value="2">Cartão de crédito</option>
+          <option value="2">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
+  functionTag() {
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select
+          id="tag"
+          data-testid="tag-input"
+          onChange={ this.handleChange }
+        >
+          <option value="1">Alimentação</option>
+          <option value="2">Lazer</option>
+          <option value="2">Trabalho</option>
+          <option value="2">Transporte</option>
+          <option value="2">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <form>
-        <label htmlFor="value">
-          Valor
-          <input
-            type="text"
-            id="value"
-            data-testid="value-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="description">
-          Descrição
-          <input
-            type="text"
-            id="description"
-            data-testid="description-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="currency">
-          Moeda
-          <select
-            id="currency"
-            data-testid="currency-input"
-            onChange={ this.handleChange }
-          >
-            <option value="1">1</option>
-          </select>
-        </label>
-        <label htmlFor="method">
-          Método de pagamento
-          <select
-            id="method"
-            data-testid="method-input"
-            onChange={ this.handleChange }
-          >
-            <option value="1">Dinheiro</option>
-            <option value="2">Cartão de crédito</option>
-            <option value="2">Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="tag">
-          Tag
-          <select
-            id="tag"
-            data-testid="tag-input"
-            onChange={ this.handleChange }
-          >
-            <option value="1">Alimentação</option>
-            <option value="2">Lazer</option>
-            <option value="2">Trabalho</option>
-            <option value="2">Transporte</option>
-            <option value="2">Saúde</option>
-          </select>
-        </label>
+        { this.funcaoValor() }
+        { this.funcaoDescricao() }
+        { this.funcaoMoeda() }
+        { this.funcaoMetodo() }
+        { this.funcaoTag() }
       </form>
     );
   }
