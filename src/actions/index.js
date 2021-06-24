c39373be225a1fa7ebee08b9ab0d1c6510faf086
CamilaCode7 <CamilaCode7>
2021-06-24 13:40:27 -0300

    Requisito 1, 2, 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const salvaLoginAction = (email) => ({
+  type: 'LOGIN',
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { salvaLoginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDisable: true,
+      email: '',
+      password: '',
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleLogin() {
+    const { email, password } = this.state;
+    const emailReg = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const passwordLength = 6;
+    if (emailReg.test(email) && password.length >= passwordLength) {
+      this.setState({ isDisable: false });
+    } else {
+      this.setState({ isDisable: true });
+    }
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value }, () => this.handleLogin());
+  }
+
   render() {
-    return <div>Login</div>;
+    const { salvaLogin } = this.props;
+    const { isDisable, email, password } = this.state;
+    return (
+      <section>
+        <p>Login</p>
+        <form>
+          <label htmlFor="email-input">
+            Email
+            <input
+              data-testid="email-input"
+              type="email"
+              value={ email }
+              name="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ isDisable }
+              inClick={ () => salvaLogin(email) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </section>
+    );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  salvaLogin: (email) => dispatch(salvaLoginAction(email)),
+});
 
-export default Login;
+Login.propTypes = {
+  salvaLogin: PropTypes.func.isRequired,
+};
+
+// https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+// https://github.com/tryber/sd-010-a-project-trybewallet/blob/michael-petterson/src/components/Header.jsx
+export default connect(null, mapDispatchToProps)(Login);
