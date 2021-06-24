0579cf76d3d8168c83e5b7c8f9feca3a7b19b39a
Matheus Gabriel Pereira do Couto <70240441+MatheusGabrielCouto@users.noreply.github.com>
2021-06-24 11:01:01 -0400

    Requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useSelector } from 'react-redux';
 
 function Wallet() {
+  const [field, setField] = useState(0);
+  const [cambio, setCambio] = useState('BRL')
+  const emailUser = useSelector(state => state.user.email);
+
   return (
     <>
       <header>
-        <h2 data-testid="email-field">Teste</h2>
-        <h3 data-testid="total-field">Teste</h3>
+        <h2 data-testid="email-field">{emailUser}</h2>
+        <h3 data-testid="total-field">{field}</h3>
+        <h3 data-testid="header-currency-field">{cambio}</h3>
       </header>
       <section>
         <h1>Ola</h1>
