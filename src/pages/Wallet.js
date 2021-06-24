f0c218ef9385f999c43a23a3258af7e6f7d5e764
Aladino Borges <eualadino@gmail.com>
2021-06-24 00:03:53 -0300

    Refatora o componente wallet para guardar no state as currencies.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,11 +4,40 @@ import Header from '../components/Header';
 import Form from '../components/Form';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getData = this.getData.bind(this);
+
+    this.state = {
+      currencies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const URL = 'https://economia.awesomeapi.com.br/json/all';
+    const fetchAPI = await fetch(URL);
+    const parseJSON = await fetchAPI.json();
+
+    const getCurrencies = Object.keys(parseJSON);
+    const removedUSDT = getCurrencies.filter((target) => target !== 'USDT');
+
+    this.setState({
+      currencies: [...removedUSDT],
+    });
+  }
+
   render() {
+    const { currencies } = this.state;
+
     return (
       <div className="wallet-page">
         <Header />
-        <Form />
+        <Form currencies={ currencies } />
       </div>);
   }
 }
