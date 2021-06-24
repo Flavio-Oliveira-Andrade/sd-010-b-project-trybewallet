1cc16c7d456b0613925a6c6cc8ac2db2ef9d7322
Matheus Gabriel Pereira do Couto <70240441+MatheusGabrielCouto@users.noreply.github.com>
2021-06-24 11:29:01 -0400

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,6 +5,15 @@ function Wallet() {
   const [field, setField] = useState(0);
   const [cambio, setCambio] = useState('BRL');
   const emailUser = useSelector((state) => state.user.email);
+  const [form, setForm] = useState({
+    id: 0,
+    value: 0,
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+    description: '',
+    exchangeRates: {},
+  })
 
   return (
     <>
@@ -13,17 +22,43 @@ function Wallet() {
         <h3 data-testid="total-field">{field}</h3>
         <h3 data-testid="header-currency-field">{cambio}</h3>
       </header>
-      <section>
-        <button
-          type="button"
-          onClick={ () => {
-            setCambio('BRL');
-            setField(0);
-          } }
-        >
-          Ola
-        </button>
-      </section>
+
+      <form>
+        <label htmlFor="value">
+          Valor:
+          <input type="number" id="value" name="value" />
+        </label>
+        <label htmlFor="moeda">
+          Moeda:
+          <select name="moeda" id="moeda">
+            <option value="BRL">BRL</option>
+            <option value="USD">USD</option>
+          </select>
+        </label>
+        <label htmlFor="method">
+          Método de pagamento:
+          <select name="method" id="method">
+            <option value="money">Dinheiro</option>
+            <option value="cred">Cartão de crédito</option>
+            <option value="deb">Cartão de debito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag:
+          <select name="tag" id="tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <label>
+          Descrição:
+          <input type="text" name="name" />
+        </label>
+      </form>
+
     </>
   );
 }
