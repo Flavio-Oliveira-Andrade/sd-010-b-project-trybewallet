c86e3a2448801193e2a886ac1203dee3658eb142
Aladino Borges <eualadino@gmail.com>
2021-06-24 00:02:42 -0300

    O componente Header pega o total de forma dinâmica.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,33 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.totalExpenses = this.totalExpenses.bind(this);
+  }
+
+  totalExpenses() {
+    let total = 0;
+    const { expenses } = this.props;
+
+    expenses.forEach(({ value, currency, exchangeRates }) => {
+      total += exchangeRates[currency].ask * value;
+    });
+
+    return total;
+  }
+
   render() {
     const { email } = this.props;
+
     return (
       <header>
         <p data-testid="email-field">
           { email }
         </p>
         <p data-testid="total-field">
-          { 0 }
+          { this.totalExpenses() }
         </p>
         <p data-testid="header-currency-field">
           BRL
@@ -27,6 +45,7 @@ Header.propTypes = {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps, null)(Header);
