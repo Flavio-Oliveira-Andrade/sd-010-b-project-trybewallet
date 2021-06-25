1de65d2b7caa9f6aa06730e46b5f56eb30fd21e2
thosijulio <thosijulio@gmail.com>
2021-06-25 08:03:33 -0300

    resolucao exercicio 2 e 3

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const userAction = (state) => ({
+  type: 'CHANGE-EMAIL',
+  payload: {
+    email: state,
+  },
+});
+
+export const teste = 1;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
+import { userAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: false,
+      email: '',
+      redirect: false,
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(eve) {
+    const email = eve.target.value;
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    this.setState({
+      login: regex.test(email),
+      email,
+    });
+  }
+
+  handlePasswordChange(eve) {
+    const password = eve.target.value;
+    const six = 6;
+    if (password.length >= six) {
+      this.setState((oldState) => ({
+        ...oldState,
+        login: true,
+      }));
+    } else {
+      this.setState({ login: false });
+    }
+  }
+
+  handleSubmit() {
+    const { email, login } = this.state;
+    if (login) {
+      const { dispatchUser } = this.props;
+      dispatchUser(email);
+      this.setState((oldState) => ({
+        ...oldState,
+        redirect: true,
+      }));
+    }
+  }
+
   render() {
+    const { redirect } = this.state;
     return (
       <main>
         <label htmlFor="email-input">
@@ -13,6 +65,7 @@ class Login extends React.Component {
             type="email"
             placeholder="trybe@betrybe.com"
             required
+            onChange={ this.handleEmailChange }
           />
         </label>
         <label htmlFor="password-input">
@@ -22,14 +75,26 @@ class Login extends React.Component {
             name="password-input"
             id="password-input"
             type="password"
-            placeholder="trybe@betrybe.com"
+            placeholder="Digite sua senha"
             required
+            onChange={ this.handlePasswordChange }
           />
         </label>
-        <button type="button">Entrar</button>
+        <button type="button" onClick={ this.handleSubmit }>Entrar</button>
+        {redirect && <Redirect to="/carteira" />}
       </main>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchUser: (email) => dispatch(
+    userAction(email),
+  ),
+});
+
+Login.propTypes = {
+  dispatchUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
