0a6b3fe85cac1ee1a48bd4f3d7e71c81f6d69592
Matheus Gabriel Pereira do Couto <70240441+MatheusGabrielCouto@users.noreply.github.com>
2021-06-25 09:46:54 -0400

    Requisito 8

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 // import { Container } from './styles';
-function inputMethod() {
+function inputMethod(tableItem, setTableItem) {
   return (
     <label className="labels-form" htmlFor="payment">
       Método de pagamento
@@ -8,16 +8,22 @@ function inputMethod() {
         className="inputs-form"
         id="payment"
         name="method"
+        onChange={ ({ target }) => {
+          setTableItem({
+            ...tableItem,
+            method: target.value,
+          });
+        } }
       >
-        <option>Dinheiro</option>
-        <option>Cartão de crédito</option>
-        <option>Cartão de débito</option>
+        <option value="Dinheiro">Dinheiro</option>
+        <option value="Cartão de crédito">Cartão de crédito</option>
+        <option value="Cartão de débito">Cartão de débito</option>
       </select>
     </label>
   );
 }
 
-function inputTag() {
+function inputTag(tableItem, setTableItem) {
   return (
     <label className="labels-form" htmlFor="tag">
       Tag
@@ -25,57 +31,108 @@ function inputTag() {
         className="inputs-form"
         id="tag"
         name="tag"
+        onChange={ ({ target }) => {
+          setTableItem({
+            ...tableItem,
+            tag: target.value,
+          });
+        } }
       >
-        <option>Alimentação</option>
-        <option>Lazer</option>
-        <option>Trabalho</option>
-        <option>Transporte</option>
-        <option>Saúde</option>
+        <option value="Alimentação">Alimentação</option>
+        <option value="Lazer">Lazer</option>
+        <option value="Trabalho">Trabalho</option>
+        <option value="Transporte">Transporte</option>
+        <option value="Saúde">Saúde</option>
       </select>
     </label>
   );
 }
 
-function From() {
-  const [currency, setCurrency] = useState([]);
+function value(tableItem, setTableItem) {
+  return (
+    <label htmlFor="value">
+      Valor:
+      <input
+        onChange={ ({ target }) => {
+          const number = parseFloat(target.value);
+          setTableItem({
+            ...tableItem,
+            value: number,
+          });
+        } }
+        type="number"
+        id="value"
+        name="value"
+      />
+    </label>
+  );
+}
 
+function description(tableItem, setTableItem) {
+  return (
+    <label className="labels-form" htmlFor="description">
+      Descrição
+      <input
+        value={ tableItem.description }
+        onChange={ ({ target }) => {
+          setTableItem({
+            ...tableItem,
+            description: target.value,
+          });
+          console.log(tableItem);
+        } }
+        type="text"
+        id="description"
+      />
+    </label>
+  );
+}
+
+function From() {
+  const [currencyState, setCurrencyState] = useState([]);
+  const [tableItem, setTableItem] = useState({
+    id: 0,
+    value: 0,
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+  });
   async function getApi() {
     const JSON = await fetch('https://economia.awesomeapi.com.br/json/all');
     const cotation = await JSON.json();
     const currencies = Object.keys(cotation)
       .filter((item) => item !== 'USDT');
-    setCurrency(currencies);
+    setCurrencyState(currencies);
   }
-
   useEffect(() => {
     getApi();
   }, []);
   return (
     <form>
-      <label htmlFor="value">
-        Valor:
-        <input type="number" id="value" name="value" />
-      </label>
+      {value(tableItem, setTableItem)}
       <label className="labels-form" htmlFor="currencie">
         Moeda
         <select
           className="inputs-form"
           id="currencie"
           name="currency"
+          onChange={ ({ target }) => {
+            setTableItem({
+              ...tableItem,
+              currency: target.value,
+            });
+          } }
         >
-          {currency.map((item) => <option key={ item } value={ item }>{item}</option>)}
+          {currencyState.map(
+            (item) => <option key={ item } value={ item }>{item}</option>,
+          )}
         </select>
       </label>
-      { inputMethod() }
-      {inputTag()}
-      <label className="labels-form" htmlFor="description">
-        Descrição
-        <input
-          type="text"
-          id="description"
-        />
-      </label>
-
+      { inputMethod(tableItem, setTableItem) }
+      {inputTag(tableItem, setTableItem)}
+      {description(tableItem, setTableItem)}
+      <button type="button">Adicionar</button>
     </form>
   );
 }
