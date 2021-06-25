0413d7039555f119a8aa2fed7051166d5c6ba03d
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-06-25 14:06:42 -0300

    requisito 10

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { dellExpense } from '../actions'
+import { dellExpense } from '../actions';
 
 class Table extends React.Component {
   constructor(props) {
