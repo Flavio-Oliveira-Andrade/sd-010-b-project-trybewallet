a50fa6fac4ca5e338441fc6fb49ccfce801c2b36
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-06-25 10:30:20 -0300

    requisito 1 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={
+            (event) => this.setState({ email: event.target.value })
+          }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={
+            (event) => this.setState({ password: event.target.value })
+          }
+        />
+
+        <Link to="/carteira">
+          <button
+            type="submit"
+            onClick={
+              () => console.log(email, password)
+            }
+          >
+            Entrar
+          </button>
+        </Link>
+
+      </div>
+    );
   }
 }
 
