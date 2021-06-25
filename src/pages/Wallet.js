d5d16afe5e7d1aaf9453fac7491fd9b582fcf2b6
thosijulio <thosijulio@gmail.com>
2021-06-25 09:07:50 -0300

    resolucao exercicio 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import './wallet.css';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <>
+        <header>
+          <h4 data-testid="email-field">
+            {` ${email}`}
+          </h4>
+          <h4 data-testid="total-field">
+            0
+          </h4>
+          <h4 data-testid="header-current-field">
+            BRL
+          </h4>
+        </header>
+        <div />
+      </>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
