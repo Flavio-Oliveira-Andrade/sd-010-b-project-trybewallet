2d02cfe3077dea2e1a1ee367b482051bd156f6f4
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-06-25 13:37:59 -0300

    requisito 7 refatorado

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
   email: '',
 };
 
-const userReducer = (state = INITIAL_STATE, action) => {
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case LOGIN:
     return { email: action.payload.email };
@@ -13,4 +13,4 @@ const userReducer = (state = INITIAL_STATE, action) => {
   }
 };
 
-export default userReducer;
+export default user;
