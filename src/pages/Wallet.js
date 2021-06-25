b9f7bdd75bffe1a4650136ef50f6b9b69ef39e21
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-06-25 02:58:01 -0300

    ESLINT FIX

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { removeExpense, editExp, loadData } from '../actions';
+import { editExp, loadData } from '../actions';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: '',
-      currency: 'BRL',
-      method: '',
-      tag: '',
-      description: '',
-      id: false,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   // this.state = {
+  //   //   value: '',
+  //   //   currency: 'BRL',
+  //   //   method: '',
+  //   //   tag: '',
+  //   //   description: '',
+  //   //   id: false,
+  //   // };
+  // }
 
   componentDidMount() {
     const { fetchAPI } = this.props;
@@ -22,7 +22,7 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email, currencies, expenses, total } = this.props;
+    const { email, total } = this.props;
     return (
       <div>
         <h1 data-testid="email-field">{ email }</h1>
@@ -48,12 +48,12 @@ const mapDispatchToProps = (dispatch) => ({
 
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
-  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
-  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  // expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
   total: PropTypes.number,
   fetchAPI: PropTypes.func.isRequired,
-  addingExpenses: PropTypes.func.isRequired,
-  editingExpenses: PropTypes.func.isRequired,
+  // addingExpenses: PropTypes.func.isRequired,
+  // editingExpenses: PropTypes.func.isRequired,
 };
 
 Wallet.defaultProps = {
